5024f99d08f21097056746e11e8c7d3cc8988516
ouyh18 <ouyh18@189.cn>
2016-05-23 15:46:11 +0800

    2016-5-23 use directive to show coverage table.

--- LtePlatform/Scripts/rutrace/coverage/controller.js
+++ LtePlatform/Scripts/rutrace/coverage/controller.js
@@ -1,4 +1,4 @@
-﻿app.controller("rutrace.coverage", function($scope, $timeout, $routeParams, $uibModal, $log, topPreciseService) {
+﻿app.controller("rutrace.coverage", function($scope, $routeParams, $uibModal, topPreciseService) {
     $scope.currentCellName = $routeParams.name + "-" + $routeParams.sectorId;
     $scope.page.title = "TOP指标覆盖分析: " + $scope.currentCellName;
     var lastWeek = new Date();
@@ -11,38 +11,13 @@
         value: new Date(),
         opened: false
     };
+    $scope.detailsDialogTitle = $routeParams.name + "-" + $routeParams.sectorId + "详细小区统计";
+    $scope.cellId = $routeParams.cellId;
+    $scope.sectorId = $routeParams.sectorId;
     $scope.showCoverage = function() {
         topPreciseService.queryCoverage($scope.beginDate.value, $scope.endDate.value,
             $routeParams.cellId, $routeParams.sectorId).then(function(result) {
             $scope.coverageList = result;
-        });
-    };
-    $scope.showDetails = function(date) {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            templateUrl: '/appViews/Rutrace/Coverage/DetailsChartDialog.html',
-            controller: 'coverage.details.dialog',
-            size: 'lg',
-            resolve: {
-                dialogTitle: function () {
-                    return $routeParams.name + "-" + $routeParams.sectorId + "详细小区统计";
-                },
-                cellId: function () {
-                    return $routeParams.cellId;
-                },
-                sectorId: function () {
-                    return $routeParams.sectorId;
-                },
-                date: function() {
-                    return date;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (info) {
-            console.log(info);
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
         });
     };
     $scope.showCoverage();
--- LtePlatform/directives/rutrace/rutrace.module.js
+++ LtePlatform/directives/rutrace/rutrace.module.js
@@ -1,6 +1,6 @@
-﻿angular.module('rutrace.module', ['kpi.workitem'])
-    .directive('topCell', function (workitemService) {
-        var htmlRoot = 'directives/rutrace/';
+﻿angular.module('rutrace.module', ['kpi.workitem', "ui.bootstrap"])
+    .constant('htmlRoot', 'directives/rutrace/')
+    .directive('topCell', function(workitemService, htmlRoot) {
         return {
             restrict: 'ECMA',
             replace: true,
@@ -12,9 +12,9 @@
                 updateMessages: '='
             },
             templateUrl: htmlRoot + 'TopCell.Tpl.html',
-            link: function (scope, element, attrs) {
-                scope.createWorkitem = function (cell) {
-                    workitemService.constructPreciseItem(cell, scope.beginDate.value, scope.endDate.value).then(function (result) {
+            link: function(scope, element, attrs) {
+                scope.createWorkitem = function(cell) {
+                    workitemService.constructPreciseItem(cell, scope.beginDate.value, scope.endDate.value).then(function(result) {
                         if (result) {
                             scope.updateMessages.push({
                                 cellName: result
@@ -25,4 +25,47 @@
                 };
             }
         };
+    })
+    .directive('coverageTable', function ($uibModal, $log, htmlRoot) {
+        return {
+            restrict: 'ECMA',
+            replace: true,
+            scope: {
+                coverageList: '=',
+                dialogTitle: '=',
+                cellId: '=',
+                sectorId: '='
+            },
+            templateUrl: htmlRoot + 'CoverageTable.Tpl.html',
+            link: function(scope, element, attrs) {
+                scope.showDetails = function(date) {
+                    var modalInstance = $uibModal.open({
+                        animation: true,
+                        templateUrl: '/appViews/Rutrace/Coverage/DetailsChartDialog.html',
+                        controller: 'coverage.details.dialog',
+                        size: 'lg',
+                        resolve: {
+                            dialogTitle: function() {
+                                return scope.dialogTitle;
+                            },
+                            cellId: function() {
+                                return scope.cellId;
+                            },
+                            sectorId: function() {
+                                return scope.sectorId;
+                            },
+                            date: function() {
+                                return date;
+                            }
+                        }
+                    });
+
+                    modalInstance.result.then(function(info) {
+                        console.log(info);
+                    }, function() {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    });
+                }
+            }
+        }
     });
