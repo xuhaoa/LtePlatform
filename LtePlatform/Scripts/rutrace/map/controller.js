3dc93c9ae1c365e235746a2e32b795e22204ab78
ouyh18 <ouyh18@189.cn>
2016-05-26 17:41:32 +0800

    2016-5-26 Add date-span directive to map.

--- LtePlatform/Scripts/rutrace/map/controller.js
+++ LtePlatform/Scripts/rutrace/map/controller.js
@@ -5,6 +5,16 @@
     $scope.displayNeighbors = false;
     $scope.reverseNeighborLines = [];
     $scope.displayReverseNeighbors = false;
+    var lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 7);
+    $scope.beginDate = {
+        value: lastWeek,
+        opened: false
+    };
+    $scope.endDate = {
+        value: new Date(),
+        opened: false
+    };
     menuItemService.updateMenuItem($scope.menuItems, 1,
         $scope.page.title,
         $scope.rootPath + "baidumap/" + $routeParams.cellId + "/" + $routeParams.sectorId + "/" + $routeParams.name);
