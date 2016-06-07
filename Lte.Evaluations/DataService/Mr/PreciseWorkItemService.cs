572bd0e96eb17ea74a3538a67081e8e2060020f9
ouyh18 <ouyh18@189.cn>
2016-06-07 22:37:28 +0800

    2016-6-7 Refactor...

---
+++ Lte.Evaluations/DataService/Mr/PreciseWorkItemService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lte.MySqlFramework.Abstract;
+
+namespace Lte.Evaluations.DataService.Mr
+{
+    public class PreciseWorkItemService
+    {
+        private readonly IPreciseWorkItemCellRepository _repository;
+
+        public PreciseWorkItemService(IPreciseWorkItemCellRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
--- Lte.Evaluations/ViewModels/Precise/PreciseWorkItemDto.cs
+++ Lte.Evaluations/ViewModels/Precise/PreciseWorkItemDto.cs
@@ -27,4 +27,37 @@ namespace Lte.Evaluations.ViewModels.Precise
 
         public string WorkItemNumber { get; set; }
     }
+
+    public class PreciseInterferenceVictimDto
+    {
+        public int ENodebId { get; set; }
+
+        public byte SectorId { get; set; }
+
+        public double BackwardDb6Share { get; set; }
+
+        public double BackwardDb10Share { get; set; }
+
+        public double BackwardMod3Share { get; set; }
+
+        public double BackwardMod6Share { get; set; }
+    }
+
+    public class PreciseInterferenceVictimsContainer
+    {
+        public List<PreciseInterferenceVictimDto> PreciseInterferenceVictims { get; set; }
+
+        public string WorkItemNumber { get; set; }
+    }
+
+    public class PreciseCoverageDto
+    {
+        public int ENodebId { get; set; }
+
+        public byte SectorId { get; set; }
+
+        public double WeakCoverageRate { get; set; }
+
+        public double OverCoverageRate { get; set; }
+    }
 }
