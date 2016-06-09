ecaec42fc61ef687be1c4ea148c563bff1f4e0e9
ouyh18 <ouyh18@189.cn>
2016-06-09 21:59:53 +0800

    2016-6-9 Fix some bugs of duplicating the controller names.

--- LtePlatform/Controllers/Kpi/WorkItemController.cs
+++ LtePlatform/Controllers/Kpi/WorkItemController.cs
@@ -159,39 +159,4 @@ namespace LtePlatform.Controllers.Kpi
             return await _service.QueryTotalItemsThisMonth();
         }
     }
-
-    [ApiControl("精确覆盖率类工单查询控制器")]
-    [Authorize]
-    public class PreciseWorkItemController : ApiController
-    {
-        private readonly WorkItemService _service;
-
-        public PreciseWorkItemController(WorkItemService service)
-        {
-            _service = service;
-        }
-
-        [HttpPost]
-        [ApiDoc("创建精确覆盖率工单")]
-        [ApiParameterDoc("view", "精确覆盖率的工单信息，包括小区信息、精确覆盖率信息、统计起止日期等")]
-        [ApiResponse("创建是否成功，如成功返回工单编号，否则返回空值")]
-        public async Task<string> Post(PreciseImportView view)
-        {
-            return await _service.ConstructPreciseWorkItem(view.View, view.Begin, view.End, User.Identity.Name);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end)
-        {
-            return await _service.QueryPreciseViews(begin, end);
-        }
-
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end, string district)
-        {
-            return await _service.QueryPreciseViews(begin, end, district);
-        }
-    }
 }
--- LtePlatform/Controllers/Mr/PreciseWorkItemController.cs
+++ LtePlatform/Controllers/Mr/PreciseWorkItemController.cs
@@ -5,19 +5,26 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Lte.Evaluations.DataService;
 using Lte.Evaluations.DataService.Mr;
+using Lte.Evaluations.ViewModels.Kpi;
 using Lte.Evaluations.ViewModels.Precise;
 using Lte.MySqlFramework.Entities;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Mr
 {
+    [ApiControl("精确覆盖率类工单查询控制器")]
+    [Authorize]
     public class PreciseWorkItemController : ApiController
     {
         private readonly PreciseWorkItemService _service;
+        private readonly WorkItemService _workItemService;
 
-        public PreciseWorkItemController(PreciseWorkItemService service)
+        public PreciseWorkItemController(PreciseWorkItemService service, WorkItemService workItemService)
         {
             _service = service;
+            _workItemService = workItemService;
         }
 
         [HttpGet]
@@ -30,6 +37,29 @@ namespace LtePlatform.Controllers.Mr
         public List<PreciseWorkItemCell> Get(string number)
         {
             return _service.Query(number);
+        }
+
+        [HttpPost]
+        [ApiDoc("创建精确覆盖率工单")]
+        [ApiParameterDoc("view", "精确覆盖率的工单信息，包括小区信息、精确覆盖率信息、统计起止日期等")]
+        [ApiResponse("创建是否成功，如成功返回工单编号，否则返回空值")]
+        public async Task<string> Post(PreciseImportView view)
+        {
+            return await _workItemService.ConstructPreciseWorkItem(view.View, view.Begin, view.End, User.Identity.Name);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end)
+        {
+            return await _workItemService.QueryPreciseViews(begin, end);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end, string district)
+        {
+            return await _workItemService.QueryPreciseViews(begin, end, district);
         }
     }
 
