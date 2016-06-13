0392bcac92750fa93bc288f34a581ae2093ce43e
ouyh18 <ouyh18@189.cn>
2016-06-13 16:30:08 +0800

    2016-6-13 Construct workitem cdma list.

--- LtePlatform/Scripts/kpi/workitem/bts.js
+++ LtePlatform/Scripts/kpi/workitem/bts.js
@@ -1,16 +1,9 @@
-﻿app.controller('workitem.bts', function ($scope, networkElementService, $routeParams, workitemService,
-    workItemDialog) {
+﻿app.controller('workitem.bts', function ($scope, networkElementService, $routeParams, workitemService) {
     $scope.serialNumber = $routeParams.serialNumber;
     $scope.queryWorkItems = function () {
         workitemService.queryByENodebId($routeParams.btsId).then(function (result) {
             $scope.viewItems = result;
         });
-    };
-    $scope.feedback = function (view) {
-        workItemDialog.feedback(view, $scope.queryWorkItems);
-    };
-    $scope.showDetails = function (view) {
-        workItemDialog.showDetails(view);
     };
     networkElementService.queryBtsInfo($routeParams.btsId).then(function (result) {
         $scope.btsDetails = result;
--- LtePlatform/Scripts/kpi/workitem/cdmaCell.js
+++ LtePlatform/Scripts/kpi/workitem/cdmaCell.js
@@ -1,16 +1,9 @@
-﻿app.controller('workitem.cdmaCell', function ($scope, networkElementService, $routeParams, workitemService,
-    workItemDialog) {
+﻿app.controller('workitem.cdmaCell', function ($scope, networkElementService, $routeParams, workitemService) {
     $scope.serialNumber = $routeParams.serialNumber;
     $scope.queryWorkItems = function () {
         workitemService.queryByCellId($routeParams.btsId, $routeParams.sectorId).then(function (result) {
             $scope.viewItems = result;
         });
-    };
-    $scope.feedback = function (view) {
-        workItemDialog.feedback(view, $scope.queryWorkItems);
-    };
-    $scope.showDetails = function (view) {
-        workItemDialog.showDetails(view);
     };
     networkElementService.queryCdmaCellInfo($routeParams.btsId, $routeParams.sectorId).then(function (result) {
         $scope.cdmaCellDetails = result;
