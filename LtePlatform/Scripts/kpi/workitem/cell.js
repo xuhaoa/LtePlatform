bc84d33a188a28cb909b008ee384f783763604dc
ouyh18 <ouyh18@189.cn>
2016-06-13 16:15:38 +0800

    2016-6-13 WorkItem cell info: test feedback.

--- LtePlatform/Scripts/kpi/workitem/cell.js
+++ LtePlatform/Scripts/kpi/workitem/cell.js
@@ -1,16 +1,9 @@
-﻿app.controller('workitem.cell', function ($scope, networkElementService, $routeParams, workitemService,
-    workItemDialog) {
+﻿app.controller('workitem.cell', function ($scope, networkElementService, $routeParams, workitemService) {
     $scope.serialNumber = $routeParams.serialNumber;
     $scope.queryWorkItems = function () {
         workitemService.queryByCellId($routeParams.eNodebId, $routeParams.sectorId).then(function (result) {
             $scope.viewItems = result;
         });
-    };
-    $scope.feedback = function (view) {
-        workItemDialog.feedback(view, $scope.queryWorkItems);
-    };
-    $scope.showDetails = function (view) {
-        workItemDialog.showDetails(view);
     };
     networkElementService.queryCellInfo($routeParams.eNodebId, $routeParams.sectorId).then(function (result) {
         $scope.lteCellDetails = result;
