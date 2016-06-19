7357122cf40d7367a8f6df2a3905636c651e3eb9
ouyh18 <ouyh18@189.cn>
2016-06-19 22:45:13 +0800

    2016-6-19 Refactor...

--- LtePlatform/Scripts/customer/route.controller.js
+++ LtePlatform/Scripts/customer/route.controller.js
@@ -1,1 +1,129 @@
-﻿
+﻿app.config([
+        '$routeProvider', function ($routeProvider) {
+            var viewDir = "/appViews/Customer/";
+            $routeProvider
+                .when('/', {
+                    templateUrl: viewDir + "Index.html",
+                    controller: "customer.index"
+                })
+                .when('/trend', {
+                    templateUrl: viewDir + "Trend.html",
+                    controller: "rutrace.trend"
+                })
+                .when('/top', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top"
+                })
+                .when('/topDistrict/:district', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top.district"
+                })
+                .when('/chart', {
+                    templateUrl: viewDir + "Chart.html",
+                    controller: "rutrace.chart"
+                })
+                .when('/trendchart', {
+                    templateUrl: viewDir + "TrendChart.html",
+                    controller: "rutrace.trendchart"
+                })
+                .when('/top', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top"
+                })
+                .when('/import/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Import.html",
+                    controller: "rutrace.import"
+                })
+                .when('/interference/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Interference/Index.html",
+                    controller: "rutrace.interference"
+                })
+                .when('/coverage/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Coverage/Index.html",
+                    controller: "rutrace.coverage"
+                })
+                .when('/baidumap/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Map/Index.html",
+                    controller: "rutrace.map"
+                })
+                .when('/details/:number', {
+                    templateUrl: viewDir + "WorkItem/AnalyticDetails.html",
+                    controller: "workitem.details"
+                })
+                .when('/workItems/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + 'WorkItem/ForCell.html',
+                    controller: "rutrace.workitems"
+                })
+                .when('/workitemDistrict/:district', {
+                    templateUrl: viewDir + "WorkItem/ForCity.html",
+                    controller: "workitem.district"
+                })
+                .when('/workitemCity', {
+                    templateUrl: viewDir + "WorkItem/ForCity.html",
+                    controller: "workitem.city"
+                })
+                .when('/cellTrend/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "WorkItem/CellTrend.html",
+                    controller: "cell.trend"
+                })
+                .when('/mongo', {
+                    templateUrl: viewDir + 'FromMongo.html',
+                    controller: 'interference.mongo'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+])
+    .run(function ($rootScope) {
+        var rootUrl = "/Customer#";
+        $rootScope.menuItems = [
+            {
+                displayName: "日常支撑",
+                isActive: true,
+                subItems: [
+                    {
+                        displayName: "指标总体情况",
+                        url: rootUrl + "/"
+                    }, {
+                        displayName: "指标变化趋势",
+                        url: rootUrl + "/trend"
+                    }
+                ]
+            }, {
+                displayName: "专项支撑",
+                isActive: false,
+                subItems: [{
+                    displayName: "应急需求",
+                    url: rootUrl + "/emergency",
+                    tooltip: "应急需求（围绕应急通信车）的申请和查询"
+                }, {
+                    displayName: "小区流量分析",
+                    url: "/Kpi/Flow"
+                }, {
+                    displayName: "校园网专题优化",
+                    url: "/College/Map",
+                    tooltip: "校园网专项优化，包括数据管理、指标分析、支撑工作管理和校园网覆盖呈现"
+                }]
+            }, {
+                displayName: "统计分析",
+                isActive: true,
+                subItems: [
+                    {
+                        displayName: "分析",
+                        url: rootUrl + "/top"
+                    }
+                ]
+            }, {
+                displayName: "基础信息",
+                isActive: false,
+                subItems: [
+                    {
+                        displayName: "从MongoDB导入",
+                        url: rootUrl + "/mongo"
+                    }
+                ]
+            }
+        ];
+        $rootScope.rootPath = rootUrl + "/";
+    });
