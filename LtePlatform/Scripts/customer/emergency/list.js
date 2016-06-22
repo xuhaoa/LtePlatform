43f9871465ca37dd8a12dc599404c436e1c8f243
ouyh18 <ouyh18@189.cn>
2016-06-22 19:54:24 +0800

    2016-6-22 Construct district options.

--- LtePlatform/Scripts/customer/emergency/list.js
+++ LtePlatform/Scripts/customer/emergency/list.js
@@ -1,5 +1,16 @@
-﻿app.controller("emergency.list", function ($scope, customerDiloagService) {
+﻿app.controller("emergency.list", function ($scope, customerDiloagService, appRegionService) {
     $scope.construct = function() {
         customerDiloagService.constructEmergencyCommunication($scope.beginDate.value, $scope.endDate.value);
     };
+
+    appRegionService.initializeCities().then(function(cities) {
+        $scope.city.options = cities;
+        $scope.city.selected = cities[0];
+        appRegionService.queryDistricts($scope.city.selected).then(function(districts) {
+            $scope.district = {
+                options: districts,
+                selected: districts[0]
+            };
+        });
+    });
 });
