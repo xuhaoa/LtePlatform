ed8e458d65323605cf7a8a2869b4772c7eec3651
ouyh18 <ouyh18@189.cn>
2016-06-29 17:18:22 +0800

    2016-6-29 Refactor...

---
+++ LtePlatform/Scripts/customer/vip/process.js
@@ -0,0 +1,19 @@
+﻿app.controller("vip.process", function ($scope, $routeParams, customerDialogService, customerQueryService) {
+    $scope.construct = function() {
+        customerDialogService.constructEmergencyCommunication(
+            $scope.city, $scope.district, $scope.type, $scope.page.messages, $scope.query);
+    };
+    $scope.query = function() {
+        customerQueryService.queryAllVip($scope.beginDate.value, $scope.endDate.value).then(function(items) {
+            $scope.items = items;
+        });
+    };
+
+    customerQueryService.queryNetworkTypeOptions().then(function(options) {
+        $scope.type = {
+            options: options,
+            selected: options[2]
+        };
+    });
+    $scope.query();
+});
