ba7ee555e2a1dc316a6d9d58e7310d32796c130d
ouyh18 <ouyh18@189.cn>
2016-06-30 22:32:58 +0800

    2016-6-30 Refactor...

--- LtePlatform/Scripts/customer/vip/process.js
+++ LtePlatform/Scripts/customer/vip/process.js
@@ -3,6 +3,12 @@
     $scope.query = function() {
         customerQueryService.queryOneVip($routeParams.number).then(function(item) {
             $scope.item = item;
+            customerQueryService.queryMarketThemeOptions().then(function(options) {
+                $scope.marketTheme = {
+                    options: options,
+                    selected: item.marketThemeDescription
+                };
+            });
         });
     };
 
