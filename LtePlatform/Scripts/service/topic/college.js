b6cdeef78371e344ad8601c95249551ccce94b32
ouyh18 <ouyh18@189.cn>
2016-07-01 22:46:15 +0800

    2016-7-1 Refactor...

--- LtePlatform/Scripts/service/topic/college.js
+++ LtePlatform/Scripts/service/topic/college.js
@@ -1,140 +1,44 @@
 ﻿angular.module('college', ['myApp.url'])
-    .factory('collegeService', function($q, $http, appUrlService) {
+    .factory('collegeService', function (generalHttpService) {
         return {
             queryNames: function () {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeNames')
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
-                });
-                return deferred.promise;
+                return generalHttpService.getApiData('CollegeNames', {});
             },
             queryStats: function () {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeStat')
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
-                });
-                return deferred.promise;
+                return generalHttpService.getApiData('CollegeStat', {});
             },
             queryRegion: function (id) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeRegion/' + id)
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
-                });
-                return deferred.promise;
+                return generalHttpService.getApiData('CollegeRegion/' + id, {});
             },
             queryENodebs: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeENodeb/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeENodeb', {
+                    collegeName: name
                 });
-                return deferred.promise;
             },
             queryCells: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeCells/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeCells', {
+                    collegeName: name
                 });
-                return deferred.promise;
             },
             queryBtss: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeBtss/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeBtss', {
+                    collegeName: name
                 });
-                return deferred.promise;
             },
             queryCdmaCells: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeCdmaCells/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeCdmaCells', {
+                    collegeName: name
                 });
-                return deferred.promise;
             },
             queryLteDistributions: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeLteDistributions/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeLteDistributions', {
+                    collegeName: name
                 });
-                return deferred.promise;
             },
             queryCdmaDistributions: function (name) {
-                var deferred = $q.defer();
-                $http({
-                    method: 'GET',
-                    url: appUrlService.getApiUrl('CollegeCdmaDistributions/'),
-                    params: {
-                        collegeName: name
-                    }
-                }).success(function (result) {
-                    deferred.resolve(result);
-                })
-                .error(function (reason) {
-                    deferred.reject(reason);
+                return generalHttpService.getApiData('CollegeCdmaDistributions', {
+                    collegeName: name
                 });
-                return deferred.promise;
             }
         }
     });
