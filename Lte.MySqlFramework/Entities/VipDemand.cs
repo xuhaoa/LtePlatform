d09fc4b77d83360299fdffd687a2a147b30d0261
ouyh18 <ouyh18@189.cn>
2016-07-04 20:32:52 +0800

    2016-7-4 Refactor...

--- Lte.MySqlFramework/Entities/VipDemand.cs
+++ Lte.MySqlFramework/Entities/VipDemand.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities;
-using Abp.EntityFramework.AutoMapper;
 using Abp.EntityFramework.Repositories;
 using AutoMapper;
 using Lte.Domain.Common.Wireless;
