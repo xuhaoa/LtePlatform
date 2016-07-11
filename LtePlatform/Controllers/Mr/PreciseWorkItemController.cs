a51518c914843d5cfcc3331a838d078e8803c0ff
ouyh18 <ouyh18@189.cn>
2016-07-11 21:05:25 +0800

    2016-7-11 Refactor...

--- LtePlatform/Controllers/Mr/PreciseWorkItemController.cs
+++ LtePlatform/Controllers/Mr/PreciseWorkItemController.cs
@@ -28,12 +28,20 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpGet]
+        [ApiDoc("查询指定工单号码下的指定小区指标信息")]
+        [ApiParameterDoc("number", "工单编号")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiParameterDoc("sectorId", "小区编号")]
+        [ApiResponse("指定小区指标信息")]
         public PreciseWorkItemCell Get(string number, int eNodebId, byte sectorId)
         {
             return _service.Query(number, eNodebId, sectorId);
         }
 
         [HttpGet]
+        [ApiDoc("查询指定工单号码下的所有小区指标信息")]
+        [ApiParameterDoc("number", "工单编号")]
+        [ApiResponse("所有小区指标信息")]
         public List<PreciseWorkItemCell> Get(string number)
         {
             return _service.Query(number);
@@ -50,6 +58,10 @@ namespace LtePlatform.Controllers.Mr
 
         [HttpGet]
         [AllowAnonymous]
+        [ApiDoc("查询指定时间范围内的所有未完成的精确覆盖率工单")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("所有未完成的精确覆盖率工单")]
         public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end)
         {
             return await _workItemService.QueryPreciseViews(begin, end);
@@ -57,6 +69,11 @@ namespace LtePlatform.Controllers.Mr
 
         [HttpGet]
         [AllowAnonymous]
+        [ApiDoc("查询指定时间范围及指定区域内的所有未完成的精确覆盖率工单")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiParameterDoc("district", "指定区域")]
+        [ApiResponse("所有未完成的精确覆盖率工单")]
         public async Task<IEnumerable<WorkItemView>> Get(DateTime begin, DateTime end, string district)
         {
             return await _workItemService.QueryPreciseViews(begin, end, district);
@@ -64,6 +81,7 @@ namespace LtePlatform.Controllers.Mr
     }
 
     [Authorize]
+    [ApiControl("精确覆盖率工单干扰邻区查询控制器")]
     public class InterferenceNeighborWorkItemController : ApiController
     {
         private readonly PreciseWorkItemService _service;
@@ -74,6 +92,8 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpPost]
+        [ApiDoc("保存干扰邻区信息")]
+        [ApiParameterDoc("container", "干扰邻区信息容器")]
         public async Task Post(PreciseInterferenceNeighborsContainer container)
         {
             await _service.UpdateAsync(container);
@@ -81,6 +101,7 @@ namespace LtePlatform.Controllers.Mr
     }
 
     [Authorize]
+    [ApiControl("精确覆盖率工单被干扰小区查询控制器")]
     public class InterferenceVictimWorkItemController : ApiController
     {
         private readonly PreciseWorkItemService _service;
@@ -91,6 +112,8 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpPost]
+        [ApiDoc("保存被干扰小区信息")]
+        [ApiParameterDoc("container", "被干扰小区信息容器")]
         public async Task Post(PreciseInterferenceVictimsContainer container)
         {
             await _service.UpdateAsync(container);
@@ -98,6 +121,7 @@ namespace LtePlatform.Controllers.Mr
     }
 
     [Authorize]
+    [ApiControl("工单内查询覆盖情况控制器")]
     public class CoverageWorkItemController : ApiController
     {
         private readonly PreciseWorkItemService _service;
@@ -108,6 +132,8 @@ namespace LtePlatform.Controllers.Mr
         }
 
         [HttpPost]
+        [ApiDoc("保存覆盖情况信息")]
+        [ApiParameterDoc("container", "覆盖情况信息容器")]
         public async Task Post(PreciseCoveragesContainer container)
         {
             await _service.UpdateAsync(container);
