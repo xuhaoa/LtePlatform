ac8b69212c342619f037f60dfe87a0d381083ce9
ouyh18 <ouyh18@189.cn>
2016-07-13 09:26:26 +0800

    2016-7-13 Refactor...

--- LtePlatform/Controllers/Dt/FileInfoControllers.cs
+++ LtePlatform/Controllers/Dt/FileInfoControllers.cs
@@ -39,4 +39,23 @@ namespace LtePlatform.Controllers.Dt
             return _service.QueryAllList();
         }
     }
+
+    [ApiControl("各区域测试日期信息的控制器")]
+    public class AreaTestDateController : ApiController
+    {
+        private readonly AreaTestDateService _service;
+
+        public AreaTestDateController(AreaTestDateService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [ApiDoc("获得各区域测试日期信息，包括2G、3G、4G的最近一次测试日期")]
+        [ApiResponse("各区域测试日期信息，包括2G、3G、4G的最近一次测试日期")]
+        public IEnumerable<AreaTestDate> Get()
+        {
+            return _service.QueryAllList();
+        }
+    }
 }
