520bb6bab4e142e91024e3859a05cbbb2fb851c2
ouyh18 <ouyh18@189.cn>
2016-07-15 14:06:22 +0800

    2016-7-15 Complete the mappers.

--- Lte.Evaluations/MapperSerive/InfrastructureMapperService.cs
+++ Lte.Evaluations/MapperSerive/InfrastructureMapperService.cs
@@ -248,6 +248,21 @@ namespace Lte.Evaluations.MapperSerive
             Mapper.CreateMap<EmergencyProcessDto, EmergencyProcess>()
                 .ForMember(d => d.ProcessState,
                     opt => opt.MapFrom(s => s.ProcessStateDescription.GetEnumType<EmergencyState>()));
+            Mapper.CreateMap<ComplainExcel, ComplainItem>()
+                .ForMember(d => d.ComplainSource,
+                    opt => opt.MapFrom(s => s.SourceDescription.GetEnumType<ComplainSource>()))
+                .ForMember(d => d.District,
+                    opt => opt.MapFrom(s => s.Grid.StartsWith("FS") ? s.Grid.Substring(2) : s.CandidateDistrict))
+                .ForMember(d => d.ComplainReason, opt => opt.MapFrom(s => s.ReasonFirst.GetEnumType<ComplainReason>()))
+                .ForMember(d => d.ComplainSubReason,
+                    opt => opt.MapFrom(s => s.ReasonSecond.GetEnumType<ComplainSubReason>()))
+                .ForMember(d => d.NetworkType, opt => opt.MapFrom(s => s.NetworkDescription.GetEnumType<NetworkType>()))
+                .ForMember(d => d.ComplainScene, opt => opt.MapFrom(s => s.Scene.GetEnumType<ComplainScene>()))
+                .ForMember(d => d.ComplainCategory,
+                    opt => opt.MapFrom(s => s.CategoryDescription.GetEnumType<ComplainCategory>()))
+                .ForMember(d => d.IsIndoor, opt => opt.MapFrom(s => s.IndoorDescription == "室内"))
+                .ForMember(d => d.SitePosition,
+                    opt => opt.MapFrom(s => string.IsNullOrEmpty(s.Site) ? s.Position : s.Site));
         }
     }
 }
