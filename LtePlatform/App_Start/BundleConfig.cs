bd04448c857a11e6226be27cb3ab9d74af7935a6
ouyh18 <ouyh18@189.cn>
2016-07-20 21:30:00 +0800

    2016-7-20 Upgrade jQuery.

--- LtePlatform/App_Start/BundleConfig.cs
+++ LtePlatform/App_Start/BundleConfig.cs
@@ -10,59 +10,18 @@ namespace LtePlatform
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/knockout.validation.js"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/dtList").Include(
-                "~/Scripts/sammy-{version}.js",
-                "~/Scripts/app/common.js",
-                "~/Scripts/app/app.datamodel.js",
-                "~/Scripts/app/app.viewmodel.js",
-                "~/Scripts/dt/list.viewmodel.js",
-                "~/Scripts/app/_run.js"));
-           
-            bundles.Add(new ScriptBundle("~/bundles/parametersAlarmImport").Include(
-                "~/Scripts/sammy-{version}.js",
-                "~/Scripts/app/common.js",
-                "~/Scripts/app/app.datamodel.js",
-                "~/Scripts/app/app.viewmodel.js",
-                "~/Scripts/parameters/common.controller.js",
-                "~/Scripts/kpi/common.controller.js",
-                "~/Scripts/parameters/alarmimport.controller.js",
-                "~/Scripts/parameters/alarmimport.viewmodel.js",
-                "~/Scripts/app/_run.js"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/webapiBasicPost").Include(
-                "~/Scripts/sammy-{version}.js",
-                "~/Scripts/app/common.js",
-                "~/Scripts/app/app.datamodel.js",
-                "~/Scripts/app/app.viewmodel.js",
-                "~/Areas/TestPage/Scripts/basicpost.viewmodel.js",
-                "~/Scripts/app/_run.js"));
-
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.css",
-                 "~/Content/bootstrap-theme.css",
+                 "~/Content/bootstrap.min.css",
+                 "~/Content/bootstrap-theme.min.css",
                  "~/Content/Site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
