2f276101242f04bccfafe791125ad4a3d4887cae
ouyh18 <ouyh18@189.cn>
2016-07-22 13:04:32 +0800

    2016-7-22 Refactor...

--- LtePlatform/App_Start/BundleConfig.cs
+++ LtePlatform/App_Start/BundleConfig.cs
@@ -18,12 +18,7 @@ namespace LtePlatform
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.min.js",
                 "~/Scripts/respond.min.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                 "~/Content/bootstrap.min.css",
-                 "~/Content/bootstrap-theme.min.css",
-                 "~/Content/Site.css"));
-
+            
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                 "~/Content/themes/base/all.css"));
             
