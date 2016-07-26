1e7e1f1a0433aba07b77caed8cc7e7013c0e1eaa
ouyh18 <ouyh18@189.cn>
2016-07-27 07:42:17 +0800

    2016-7-27 Refactor...

--- Lte.Evaluations/DataService/College/CollegeStatService.cs
+++ Lte.Evaluations/DataService/College/CollegeStatService.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.EntityFramework.AutoMapper;
 using AutoMapper;
 using Lte.Evaluations.ViewModels.College;
+using Lte.Evaluations.ViewModels.Precise;
 using Lte.MySqlFramework.Abstract;
 using Lte.MySqlFramework.Entities;
 using Lte.Parameters.Abstract;
+using Lte.Parameters.Abstract.Basic;
 using Lte.Parameters.Abstract.College;
 using Lte.Parameters.Abstract.Infrastructure;
+using Lte.Parameters.Abstract.Kpi;
+using Lte.Parameters.Entities;
 using Lte.Parameters.Entities.College;
 
 namespace Lte.Evaluations.DataService.College
@@ -102,6 +108,138 @@ namespace Lte.Evaluations.DataService.College
                 infos.Select(x => new {x.Name, YearInfo = _yearRepository.GetByCollegeAndYear(x.Id, year)})
                     .Where(x => x.YearInfo != null);
             return query.Select(x => x.Name).Distinct();
+        }
+    }
+
+    public class CollegeKpiService
+    {
+        private readonly ICollegeKpiRepository _repository;
+        private readonly ICollegeRepository _collegeRepository;
+
+        public CollegeKpiService(ICollegeKpiRepository repository, ICollegeRepository collegeRepository)
+        {
+            _repository = repository;
+            _collegeRepository = collegeRepository;
+        }
+
+        public IEnumerable<CollegeKpiView> GetViews(DateTime date, int hour)
+        {
+            var time = date.AddHours(hour);
+            var results = _repository.GetAllList(time);
+
+            if (!results.Any()) return new List<CollegeKpiView>();
+            return results.Select(x =>
+            {
+                var college = _collegeRepository.Get(x.CollegeId);
+                var view = x.MapTo<CollegeKpiView>();
+                view.CollegeName = college?.Name;
+                return view;
+            });
+        }
+
+        public CollegeKpi GetResult(DateTime date, int hour, string name)
+        {
+            var college = _collegeRepository.GetByName(name);
+            if (college == null) return null;
+            var time = date.AddHours(hour);
+            var result = _repository.GetByCollegeIdAndTime(college.Id, time);
+            return result ?? new CollegeKpi
+            {
+                TestTime = date.AddHours(hour),
+                CollegeId = college.Id,
+                OnlineUsers = 100,
+                DownloadFlow = 10,
+                UploadFlow = 1,
+                RrcConnection = 99.9,
+                ErabConnection = 99.9,
+                ErabDrop = 0.1,
+                Connection2G = 99.8,
+                Connection3G = 99.9,
+                Erlang3G = 32,
+                Flow3G = 1,
+                Drop3G = 0.1
+            };
+        }
+
+        public Dictionary<string, double> GetAverageKpi(DateTime begin, DateTime end, string kpiName)
+        {
+            var property = typeof(CollegeKpi).GetProperty(kpiName);
+            if (property == null) return null;
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                        join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                        select new { c.Name, Kpi = (double)property.GetValue(r) };
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Kpi));
+        }
+
+        public static string GetKpiName(string tips)
+        {
+            var dictionay = new Dictionary<string, string>
+            {
+                {"在线用户数", "OnlineUsers"},
+                {"下行速率", "DownloadFlow"},
+                {"上行速率", "UploadFlow" },
+                {"rRC连接成功率", "RrcConnection" },
+                {"e-RAB连接成功率", "ErabConnection" },
+                {"e-RAB掉线率", "ErabDrop" },
+                {"呼建成功率", "Connection2G" },
+                {"3G连接成功率", "Connection3G" },
+                {"3G话务量", "Erlang3G" },
+                {"3G流量", "Flow3G" },
+                {"3G掉线率", "Drop3G" }
+            };
+            return dictionay.ContainsKey(tips) ? dictionay[tips] : "OnlineUsers";
+        }
+
+        public async Task<CollegeKpi> Post(CollegeKpi stat)
+        {
+            var item = await _repository.InsertOrUpdateAsync(stat);
+            _repository.SaveChanges();
+            return item;
+        }
+
+        public async Task<int> Delete(CollegeKpi stat)
+        {
+            await _repository.DeleteAsync(stat);
+            return _repository.SaveChanges();
+        }
+
+        public CollegeKpi GetRecordResult(CollegeKpiView view)
+        {
+            var college = _collegeRepository.GetByName(view.CollegeName);
+            if (college == null) return null;
+            var time = view.TestTime;
+            return _repository.GetByCollegeIdAndTime(college.Id, time);
+        }
+    }
+
+    public class CollegePreciseService
+    {
+        private readonly IInfrastructureRepository _repository;
+        private readonly ICellRepository _cellRepository;
+        private readonly IENodebRepository _eNodebRepository;
+        private readonly IPreciseCoverage4GRepository _kpiRepository;
+
+        public CollegePreciseService(IInfrastructureRepository repository, ICellRepository cellRepository,
+            IENodebRepository eNodebRepository, IPreciseCoverage4GRepository kpiRepository)
+        {
+            _repository = repository;
+            _cellRepository = cellRepository;
+            _eNodebRepository = eNodebRepository;
+            _kpiRepository = kpiRepository;
+        }
+
+        public IEnumerable<CellPreciseKpiView> GetViews(string collegeName, DateTime begin, DateTime end)
+        {
+            var ids = _repository.GetCollegeInfrastructureIds(collegeName, InfrastructureType.Cell);
+            var query =
+                ids.Select(_cellRepository.Get).Where(cell => cell != null)
+                    .Select(x => CellPreciseKpiView.ConstructView(x, _eNodebRepository)).ToList();
+            foreach (var view in query)
+            {
+                view.UpdateKpi(_kpiRepository, begin, end);
+            }
+            return query;
         }
     }
 }
--- Lte.Evaluations/DataService/College/CollegeTestService.cs
+++ Lte.Evaluations/DataService/College/CollegeTestService.cs
@@ -5,13 +5,119 @@ using System.Threading.Tasks;
 using Abp.EntityFramework.AutoMapper;
 using Lte.Evaluations.ViewModels;
 using Lte.Evaluations.ViewModels.College;
-using Lte.Parameters.Abstract;
 using Lte.Parameters.Abstract.Basic;
 using Lte.Parameters.Abstract.College;
 using Lte.Parameters.Entities.College;
 
 namespace Lte.Evaluations.DataService.College
 {
+    public class College3GTestService
+    {
+        private readonly ICollege3GTestRepository _repository;
+        private readonly ICollegeRepository _collegeRepository;
+
+        public College3GTestService(ICollege3GTestRepository repository, ICollegeRepository collegeRepository)
+        {
+            _repository = repository;
+            _collegeRepository = collegeRepository;
+        }
+
+        public IEnumerable<College3GTestView> GetViews(DateTime date, int hour)
+        {
+            var statTime = date.AddHours(hour);
+            var results = _repository.GetAll().Where(x => x.TestTime == statTime).ToList();
+            if (!results.Any()) return new List<College3GTestView>();
+            return results.Select(x =>
+            {
+                var college = _collegeRepository.Get(x.CollegeId);
+                var view = x.MapTo<College3GTestView>();
+                view.CollegeName = college?.Name;
+                return view;
+            });
+        }
+
+        public College3GTestResults GetResult(DateTime date, int hour, string name)
+        {
+            var college = _collegeRepository.GetByName(name);
+            if (college == null) return null;
+            var time = date.AddHours(hour);
+            var result = _repository.GetByCollegeIdAndTime(college.Id, time);
+            return result ?? new College3GTestResults
+            {
+                TestTime = date.AddHours(hour),
+                CollegeId = college.Id,
+                AccessUsers = 10,
+                DownloadRate = 500,
+                MinRssi = -110,
+                MaxRssi = -100,
+                Vswr = 1.4
+            };
+        }
+
+        public Dictionary<string, double> GetAverageRates(DateTime begin, DateTime end)
+        {
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                        join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                        select new {c.Name, Rate = r.DownloadRate };
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Rate));
+        }
+
+        public Dictionary<string, double> GetAverageUsers(DateTime begin, DateTime end)
+        {
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                select new {c.Name, Users = (double) r.AccessUsers};
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Users));
+        }
+
+        public Dictionary<string, double> GetAverageMinRssi(DateTime begin, DateTime end)
+        {
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                        join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                        select new { c.Name, Rssi = r.MinRssi };
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Rssi));
+        }
+
+        public Dictionary<string, double> GetAverageMaxRssi(DateTime begin, DateTime end)
+        {
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                        join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                        select new { c.Name, Rssi = r.MaxRssi };
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Rssi));
+        }
+
+        public Dictionary<string, double> GetAverageVswr(DateTime begin, DateTime end)
+        {
+            var results = _repository.GetAllList(begin, end);
+            var query = from r in results
+                        join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
+                        select new { c.Name, r.Vswr };
+            return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Vswr));
+        }
+
+        public async Task<int> Post(College3GTestResults result)
+        {
+            await _repository.InsertOrUpdateAsync(result);
+            return _repository.SaveChanges();
+        }
+
+        public async Task<int> Delete(College3GTestResults result)
+        {
+            await _repository.DeleteAsync(result);
+            return _repository.SaveChanges();
+        }
+
+        public College3GTestResults GetRecordResult(DateTime time, string name)
+        {
+            var college = _collegeRepository.GetByName(name);
+            return college == null ? null : _repository.GetByCollegeIdAndTime(college.Id, time);
+        }
+    }
+
     public class College4GTestService
     {
         private readonly ICollege4GTestRepository _repository;
@@ -79,7 +185,7 @@ namespace Lte.Evaluations.DataService.College
             var results = _repository.GetAllList().Where(x => x.TestTime >= begin && x.TestTime < end);
             var query = from r in results
                         join c in _collegeRepository.GetAllList() on r.CollegeId equals c.Id
-                        select new {c.Name, Rate = (upload == 0) ? r.DownloadRate : r.UploadRate };
+                        select new { c.Name, Rate = (upload == 0) ? r.DownloadRate : r.UploadRate };
             return query.GroupBy(x => x.Name).ToDictionary(s => s.Key, t => t.Average(x => x.Rate));
         }
 
