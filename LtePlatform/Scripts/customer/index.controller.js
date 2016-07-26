4b0f1c38c9748e02d543f262bb3541f640b00168
ouyh18 <ouyh18@189.cn>
2016-07-26 10:19:56 +0800

    2016-7-26 refactor...

--- LtePlatform/Scripts/customer/index.controller.js
+++ LtePlatform/Scripts/customer/index.controller.js
@@ -1,4 +1,2 @@
-﻿app.controller("customer.index", function ($scope, baiduMapService) {
-    baiduMapService.initializeMap("map", 11);
-    baiduMapService.addCityBoundary("佛山");
+﻿app.controller("customer.index", function ($scope) {
 });
