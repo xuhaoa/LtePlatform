2117ded60d74b1d8772407a1a3370621b0e73130
ouyh18 <ouyh18@189.cn>
2016-07-29 22:50:42 +0800

    2016-7-29 Add some tests.

--- Lte.Parameters.Test/Basic/EUtranRelationZteRepositoryTests.cs
+++ Lte.Parameters.Test/Basic/EUtranRelationZteRepositoryTests.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lte.Parameters.Abstract.Basic;
 using Lte.Parameters.Abstract.Neighbor;
+using Lte.Parameters.Abstract.Switch;
+using Lte.Parameters.Concrete;
+using Lte.Parameters.Concrete.Basic;
 using Lte.Parameters.Concrete.Neighbor;
+using Lte.Parameters.Concrete.Switch;
 using NUnit.Framework;
 
 namespace Lte.Parameters.Test.Basic
@@ -50,6 +55,65 @@ namespace Lte.Parameters.Test.Basic
             var result = _repository.GetRecent(eNodebId, externalId);
             Assert.IsNotNull(result);
             Assert.AreEqual(result.iDate, date);
+        }
+    }
+
+    [TestFixture]
+    public class EUtranRelationZteRepositoryTestsOuter
+    {
+        private readonly IEUtranRelationZteRepository _repository = new EUtranRelationZteRepository(new OuterMongoProvider("fangww"));
+
+        [Test]
+        public void Test_GetRecentBySectorId()
+        {
+            var results = _repository.GetRecentList(551203, 48);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Count, 189);
+            Assert.AreEqual(results[0].iDate, "20160722");
+        }
+
+        [TestCase(551203, 49, "20160722")]
+        [TestCase(502776, 148, "20160722")]
+        [TestCase(502776, 123, "20160722")]
+        [TestCase(551606, 35, "20160722")]
+        public void Test_GetRecent(int eNodebId, int externalId, string date)
+        {
+            var result = _repository.GetRecent(eNodebId, externalId);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.iDate, date);
+        }
+    }
+
+    [TestFixture]
+    public class EUtranCellFDDZteRepositoryTestsOuter
+    {
+        private readonly IEUtranCellFDDZteRepository _repository=new EUtranCellFDDZteRepository(new OuterMongoProvider("fangww"));
+
+        [Test]
+        public void Test_GetRecentBySectorId()
+        {
+            var results = _repository.GetRecent(501777, 49);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.eNodeB_Id, 501777);
+            Assert.AreEqual(results.cellLocalId, 49);
+            Assert.AreEqual(results.adminState, 0);
+        }
+
+    }
+
+    [TestFixture]
+    public class EUtranCellMeasurementZteTestsOuter
+    {
+        private readonly IEUtranCellMeasurementZteRepository _repository = new EUtranCellMeasurementZteRepository(new OuterMongoProvider("fangww"));
+
+        [Test]
+        public void Test_GetRecentBySectorId()
+        {
+            var results = _repository.GetRecent(501777, 49);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.eNodeB_Id, 501777);
+            Assert.AreEqual(results.description, "cellLocalId=49");
+            Assert.AreEqual(results.CDMA2000MeasParas_cdmaType, 0);
         }
     }
 }
--- Lte.Parameters.Test/Basic/PrachFDDZteRepositoryTests.cs
+++ Lte.Parameters.Test/Basic/PrachFDDZteRepositoryTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lte.Parameters.Abstract.Basic;
+using Lte.Parameters.Concrete;
 using Lte.Parameters.Concrete.Basic;
 using NUnit.Framework;
 
@@ -20,6 +21,21 @@ namespace Lte.Parameters.Test.Basic
             var results = _repository.GetRecent(551203, 48);
             Assert.IsNotNull(results);
             Assert.AreEqual(results.iDate, "20160408");
+        }
+    }
+
+
+    [TestFixture]
+    public class PrachFDDZteRepositoryTestsOuter
+    {
+        private readonly IPrachFDDZteRepository _repository = new PrachFDDZteRepository(new OuterMongoProvider("fangww"));
+
+        [Test]
+        public void Test_GetRecentBySectorId()
+        {
+            var results = _repository.GetRecent(501777, 49);
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.iDate, "20160722");
         }
     }
 }
