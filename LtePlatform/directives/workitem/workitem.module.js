72563ed182ca5d281ad4ee3ba9089f5342a45970
ouyh18 <ouyh18@189.cn>
2016-07-29 09:54:54 +0800

    2016-7-29 Refactor...

--- LtePlatform/directives/workitem/workitem.module.js
+++ LtePlatform/directives/workitem/workitem.module.js
@@ -1,24 +1,78 @@
-﻿angular.module('workitem.module', ['kpi.workitem'])
-    .constant('workitemRoot', '/directives/workitem/')
-    .directive('workitemTable', function(workitemRoot, workItemDialog) {
-        return {
-            restrict: 'ECMA',
-            replace: true,
-            scope: {
-                viewItems: '=',
-                queryWorkItems: '&'
-            },
-            templateUrl: workitemRoot + 'WorkItem.Tpl.html',
-            link: function(scope, element, attrs) {
-                scope.feedback = function(view) {
-                    workItemDialog.feedback(view, scope.queryWorkItems);
-                };
-                scope.showDetails = function(view) {
-                    workItemDialog.showDetails(view, scope.queryWorkItems);
-                };
-            }
+﻿angular.module('workitem.module', ['workitem.module.table', 'workitem.module.feedback', 'workitem.module.details'])
+    .constant('workitemRoot', '/directives/workitem/');
+
+angular.module('workitem.module.table', ['kpi.workitem'])
+    .controller('WorkItemTableController', function ($scope, workitemRoot, workItemDialog) {
+        $scope.gridOptions = {
+            paginationPageSizes: [20, 40, 60],
+            paginationPageSize: 20,
+            columnDefs: [
+                { field: 'district', name: '区域' },
+                {
+                    field: 'serialNumber',
+                    name: '工单编号',
+                    cellTooltip: function(row) {
+                        return '工单编号: ' + row.entity.serialNumber;
+                    } 
+                },
+                {
+                    field: 'eNodebName',
+                    name: '基站名称',
+                    cellTooltip: function (row) {
+                        return '基站名称: ' + row.entity.eNodebName;
+                    }
+                },
+                { field: 'workItemType', name: '类型' },
+                { field: 'workItemSubType', name: '子类型' },
+                { field: 'workItemState', name: '状态' },
+                { field: 'deadline', name: '时限', cellFilter: 'date: "yyyy-MM-dd"' },
+                {
+                    name: '详情',
+                    cellTemplate: '<button class="btn btn-xs btn-default" ng-click="grid.appScope.showDetails(row.entity)">详情</button>'
+                },
+                {
+                    name: '详细分析',
+                    cellTemplate: '<a ng-href="{{row.entity.detailsPath}}" ng-if="row.entity.detailsPath" class="btn btn-sm btn-success">详细分析</a>'
+                },
+                {
+                    name: '反馈',
+                    cellTemplate: '<button class="btn btn-xs btn-info" ng-click="grid.appScope.feedback(row.entity)">反馈</button>'
+                }
+            ],
+            data: []
+        };
+        $scope.feedback = function(view) {
+            workItemDialog.feedback(view, $scope.queryWorkItems);
+        };
+        $scope.showDetails = function(view) {
+            workItemDialog.showDetails(view, $scope.queryWorkItems);
         };
     })
+    .directive('workitemTable', function ($compile) {
+        return {
+            restrict: 'EA',
+            replace: true,
+            controller: 'WorkItemTableController',
+            scope: {
+                items: '=',
+                queryWorkItems: '&'
+            },
+            template: '<div></div>',
+            link: function(scope, element, attrs) {
+                scope.initialize = false;
+                scope.$watch('items', function (items) {
+                    scope.gridOptions.data = items;
+                    if (!scope.initialize) {
+                        var linkDom = $compile('<div ui-grid="gridOptions" ui-grid-pagination style="height: 600px"></div>')(scope);
+                        element.append(linkDom);
+                        scope.initialize = true;
+                    }
+                });
+            }
+        };
+    });
+
+angular.module('workitem.module.feedback', ['kpi.workitem'])
     .directive('platformAndFeedbackInfo', function(workitemRoot, workItemDialog, workitemService) {
         return {
             restrict: 'ECMA',
@@ -32,13 +86,13 @@
                 scope.feedback = function() {
                     workItemDialog.feedback(scope.currentView, scope.updateWorkItems);
                 };
-                scope.$watch('currentView', function (view) {
+                scope.$watch('currentView', function(view) {
                     if (view) {
                         scope.platformInfos = workItemDialog.calculatePlatformInfo(view.comments);
                         scope.feedbackInfos = workItemDialog.calculatePlatformInfo(view.feedbackContents);
                     }
                 });
-                
+
                 scope.updateWorkItems = function() {
                     workitemService.querySingleItem(scope.serialNumber).then(function(result) {
                         scope.currentView = result;
@@ -46,7 +100,9 @@
                 };
             }
         };
-    })
+    });
+
+angular.module('workitem.module.details', [])
     .directive('workItemDetails', function(workitemRoot) {
         return {
             restrict: 'ECMA',
