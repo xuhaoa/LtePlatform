f200bfaf17d26e09b97f2b6348e5fbd002bee57f
ouyh18 <ouyh18@189.cn>
2016-08-03 09:37:34 +0800

    2016-8-3 Refactor...

--- LtePlatform/Scripts/rutrace/index.controller.js
+++ LtePlatform/Scripts/rutrace/index.controller.js
@@ -1,29 +1,223 @@
-﻿
-app.controller("rutrace.index", function ($scope, appRegionService, appKpiService, kpiPreciseService, appFormatService) {
-    $scope.page.title = "指标总体情况";
-    var yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    $scope.statDate = {
-        value: yesterday,
-        opened: false
-    };
-    $scope.showKpi = function(city) {
-        kpiPreciseService.getRecentPreciseRegionKpi(city, $scope.statDate.value)
-            .then(function (result) {
-                $scope.statDate.value = appFormatService.getDate(result.statDate);
-                angular.forEach(result.districtPreciseViews, function(view) {
-                    view.objectRate = appKpiService.getPreciseObject(view.district);
+﻿angular.module('myApp', ['rutrace.main', 'rutrace.precise']);
+
+angular.module('rutrace.main', ['app.common'])
+    .config([
+        '$routeProvider', function($routeProvider) {
+            var viewDir = "/appViews/Rutrace/";
+            $routeProvider
+                .when('/', {
+                    templateUrl: viewDir + "Index.html",
+                    controller: "rutrace.index"
+                })
+                .when('/trend', {
+                    templateUrl: viewDir + "Trend.html",
+                    controller: "rutrace.trend"
+                })
+                .when('/top', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top"
+                })
+                .when('/topDistrict/:district', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top.district"
+                })
+                .when('/chart', {
+                    templateUrl: viewDir + "Chart.html",
+                    controller: "rutrace.chart"
+                })
+                .when('/trendchart', {
+                    templateUrl: viewDir + "TrendChart.html",
+                    controller: "rutrace.trendchart"
+                })
+                .when('/top', {
+                    templateUrl: viewDir + "Top.html",
+                    controller: "rutrace.top"
+                })
+                .when('/import/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Import.html",
+                    controller: "rutrace.import"
+                })
+                .when('/interference/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Interference/Index.html",
+                    controller: "rutrace.interference"
+                })
+                .when('/coverage/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Coverage/Index.html",
+                    controller: "rutrace.coverage"
+                })
+                .when('/baidumap/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "Map/Index.html",
+                    controller: "rutrace.map"
+                })
+                .when('/details/:number', {
+                    templateUrl: viewDir + "WorkItem/AnalyticDetails.html",
+                    controller: "workitem.details"
+                })
+                .when('/workItems/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + 'WorkItem/ForCell.html',
+                    controller: "rutrace.workitems"
+                })
+                .when('/workitemDistrict/:district', {
+                    templateUrl: viewDir + "WorkItem/ForCity.html",
+                    controller: "workitem.district"
+                })
+                .when('/workitemCity', {
+                    templateUrl: viewDir + "WorkItem/ForCity.html",
+                    controller: "workitem.city"
+                })
+                .when('/cellTrend/:cellId/:sectorId/:name', {
+                    templateUrl: viewDir + "WorkItem/CellTrend.html",
+                    controller: "cell.trend"
+                })
+                .when('/mongo', {
+                    templateUrl: viewDir + 'FromMongo.html',
+                    controller: 'interference.mongo'
+                })
+                .otherwise({
+                    redirectTo: '/'
                 });
-                $scope.overallStat.districtStats = result.districtPreciseViews;
-                $scope.overallStat.townStats = result.townPreciseViews;
-                $scope.overallStat.currentDistrict = result.districtPreciseViews[0].district;
-                $scope.overallStat.districtStats.push(appKpiService.getCityStat($scope.overallStat.districtStats, city));
-                $scope.overallStat.dateString = appFormatService.getDateString($scope.statDate.value, "yyyy年MM月dd日");
+        }
+    ])
+    .run(function($rootScope) {
+        var rootUrl = "/Rutrace#";
+        $rootScope.menuItems = [
+            {
+                displayName: "总体情况",
+                isActive: true,
+                subItems: [
+                    {
+                        displayName: "指标总体情况",
+                        url: rootUrl + "/"
+                    }, {
+                        displayName: "指标变化趋势",
+                        url: rootUrl + "/trend"
+                    }
+                ]
+            }, {
+                displayName: "详细查询",
+                isActive: false,
+                subItems: []
+            }, {
+                displayName: "TOP指标",
+                isActive: true,
+                subItems: [
+                    {
+                        displayName: "分析",
+                        url: rootUrl + "/top"
+                    }
+                ]
+            }, {
+                displayName: "辅助功能",
+                isActive: false,
+                subItems: [
+                    {
+                        displayName: "从MongoDB导入",
+                        url: rootUrl + "/mongo"
+                    }
+                ]
+            }
+        ];
+        $rootScope.rootPath = rootUrl + "/";
+
+        $rootScope.viewData = {
+            workItems: []
+        };
+        var lastSeason = new Date();
+        lastSeason.setDate(lastSeason.getDate() - 100);
+        var lastWeek = new Date();
+        lastWeek.setDate(lastWeek.getDate() - 7);
+        $rootScope.beginDate = {
+            value: new Date(lastWeek.getFullYear(), lastWeek.getMonth(), lastWeek.getDate(), 8),
+            opened: false
+        };
+        $rootScope.seasonDate = {
+            value: new Date(lastSeason.getFullYear(), lastSeason.getMonth(), lastSeason.getDate(), 8),
+            opened: false
+        };
+        var today = new Date();
+        $rootScope.endDate = {
+            value: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8),
+            opened: false
+        };
+    })
+    .controller("rutrace.root", function($scope, appRegionService, menuItemService) {
+        $scope.page = { title: "指标总体情况" };
+        $scope.overallStat = {
+            currentDistrict: "",
+            districtStats: [],
+            townStats: [],
+            cityStat: {},
+            dateString: "",
+            city: ""
+        };
+        $scope.trendStat = {
+            stats: [],
+            districts: [],
+            districtStats: [],
+            townStats: [],
+            beginDateString: "",
+            endDateString: ""
+        };
+        $scope.topStat = {
+            current: {},
+            cells: {},
+            interference: {},
+            victims: {},
+            coverages: {},
+            updateInteferenceProgress: {},
+            updateVictimProgress: {},
+            mongoNeighbors: {},
+            pieOptions: {},
+            columnOptions: {}
+        };
+        $scope.updateTopCells = function(cell) {
+            var cellName = cell.eNodebName + "-" + cell.sectorId;
+            if ($scope.topStat.cells[cellName] === undefined) {
+                $scope.topStat.cells[cellName] = cell;
+            }
+        };
+        $scope.city = {
+            selected: "",
+            options: []
+        };
+
+        appRegionService.initializeCities().then(function(result) {
+            $scope.overallStat.city = result[0];
+            $scope.city.options = result;
+            $scope.city.selected = result[0];
+            appRegionService.queryDistricts(result[0]).then(function(districts) {
+                angular.forEach(districts, function(district) {
+                    menuItemService.updateMenuItem($scope.menuItems, 2,
+                        "TOP指标分析-" + district, $scope.rootPath + "topDistrict/" + district);
+                });
             });
-    };
-    $scope.$watch('city.selected', function (city) {
-        if (city) {
-            $scope.showKpi(city);
-        }        
+        });
+    })
+    .controller("rutrace.index", function($scope, appRegionService, appKpiService, kpiPreciseService, appFormatService) {
+        $scope.page.title = "指标总体情况";
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        $scope.statDate = {
+            value: yesterday,
+            opened: false
+        };
+        $scope.showKpi = function(city) {
+            kpiPreciseService.getRecentPreciseRegionKpi(city, $scope.statDate.value)
+                .then(function(result) {
+                    $scope.statDate.value = appFormatService.getDate(result.statDate);
+                    angular.forEach(result.districtPreciseViews, function(view) {
+                        view.objectRate = appKpiService.getPreciseObject(view.district);
+                    });
+                    $scope.overallStat.districtStats = result.districtPreciseViews;
+                    $scope.overallStat.townStats = result.townPreciseViews;
+                    $scope.overallStat.currentDistrict = result.districtPreciseViews[0].district;
+                    $scope.overallStat.districtStats.push(appKpiService.getCityStat($scope.overallStat.districtStats, city));
+                    $scope.overallStat.dateString = appFormatService.getDateString($scope.statDate.value, "yyyy年MM月dd日");
+                });
+        };
+        $scope.$watch('city.selected', function(city) {
+            if (city) {
+                $scope.showKpi(city);
+            }
+        });
     });
-});
