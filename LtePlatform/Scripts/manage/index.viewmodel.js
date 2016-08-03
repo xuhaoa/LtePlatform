0a5ed58428dce4bc67dcdadaab31618fc5300381
ouyh18 <ouyh18@189.cn>
2016-08-03 17:51:21 +0800

    2016-8-3 Refactor...

--- LtePlatform/Scripts/manage/index.viewmodel.js
+++ LtePlatform/Scripts/manage/index.viewmodel.js
@@ -1,4 +1,5 @@
-﻿app.config([
+﻿angular.module('myApp', ['app.common'])
+.config([
         '$routeProvider', function($routeProvider) {
             $routeProvider
                 .when('/', {
@@ -56,4 +57,239 @@
         $rootScope.closeAlert = function (index) {
             $rootScope.page.messages.splice(index, 1);
         };
+    })
+.controller('manage.all', function ($scope, authorizeService) {
+    $scope.page.title = "所有用户信息管理";
+    $scope.manageUsers = [];
+    authorizeService.queryAllUsers().then(function (result) {
+        $scope.manageUsers = result;
+        for (var i = 0; i < result.length; i++) {
+            authorizeService.queryEmailConfirmed(result[i].userName).then(function (confirmed) {
+                for (var j = 0; j < result.length; j++) {
+                    if (result[j].userName === confirmed.Name) {
+                        $scope.manageUsers[j].emailHasBeenConfirmed = confirmed.Result;
+                        break;
+                    }
+                }
+            });
+        }
     });
+})
+.controller("manage.current", function ($scope, authorizeService) {
+    $scope.page.title = "本账号信息管理";
+    authorizeService.queryCurrentUserInfo().then(function (result) {
+        $scope.currentUser = result;
+    });
+    $scope.removePhoneNumber = function () {
+        authorizeService.removePhoneNumber().then(function (result) {
+            $scope.page.messages.push({
+                contents: result,
+                type: 'success'
+            });
+            $scope.currentUser.phoneNumber = null;
+        });
+    };
+    $scope.confirmEmail = function () {
+        authorizeService.confirmEmail($scope.currentUser).then(function (result) {
+            $scope.page.messages.push({
+                contents: result.Message,
+                type: result.Type
+            });
+        });
+    };
+})
+.controller('manage.roleUser', function ($scope, $routeParams, $http) {
+    $scope.panelTitle = $routeParams.name + "角色用户列表管理";
+})
+.controller('manage.roles', function ($scope, authorizeService) {
+    $scope.page.title = "所有角色管理";
+    $scope.manageRoles = [];
+    $scope.inputRole = {
+        name: ""
+    };
+    $scope.updateRoleList = function () {
+        authorizeService.updateRoleList().then(function (result) {
+            $scope.manageRoles = result;
+            $scope.inputRole.name = "New role " + result.length;
+        });
+    };
+    $scope.addRole = function () {
+        authorizeService.addRole($scope.inputRole.name).then(function (result) {
+            $scope.updateRoleList();
+            $scope.page.messages.push({
+                contents: result,
+                type: 'success'
+            });
+        }, function (reason) {
+            $scope.page.messages.push({
+                contents: reason,
+                type: 'warning'
+            });
+        });
+    };
+    $scope.deleteRole = function (name) {
+        authorizeService.deleteRole(name).then(function (result) {
+            $scope.updateRoleList();
+            $scope.page.messages.push({
+                contents: result,
+                type: 'success'
+            });
+        }, function (reason) {
+            $scope.page.messages.push({
+                contents: reason,
+                type: 'warning'
+            });
+        });
+    };
+
+    $scope.updateRoleList();
+})
+.controller('password.change', function ($scope, authorizeService) {
+    $scope.signupForm = function () {
+        if ($scope.signup_form.$valid) {
+            authorizeService.changePassword($scope.signup).then(function (result) {
+                $scope.page.messages.push({
+                    contents: result.Message,
+                    type: result.Type
+                });
+            }, function (reason) {
+                $scope.page.messages.push({
+                    contents: reason,
+                    type: 'warning'
+                });
+            });
+        } else {
+            $scope.page.messages.push({
+                contents: '输入密码有误！请检查。',
+                type: 'warning'
+            });
+        }
+    };
+})
+.controller('password.forgot', function ($scope, authorizeService) {
+    $scope.page = {
+        messages: []
+    };
+    $scope.signup = {
+        userName: "",
+        email: ""
+    };
+    $scope.signupForm = function () {
+        authorizeService.forgotPassword($scope.signup).then(function (result) {
+            $scope.page.messages.push({
+                contents: result.Message,
+                type: result.Type
+            });
+        }, function (reason) {
+            $scope.page.messages.push({
+                contents: reason,
+                type: 'warning'
+            });
+        });
+    };
+
+    $scope.closeAlert = function (index) {
+        $scope.page.messages.splice(index, 1);
+    };
+})
+.controller('password.reset', function ($scope, $window, authorizeService, appUrlService) {
+    $scope.signup = {
+        userName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        code: appUrlService.parseQueryString($window.location.href).code
+    };
+    $scope.page = {
+        messages: []
+    };
+    $scope.signupForm = function () {
+        $scope.signupForm = function () {
+            authorizeService.resetPassword($scope.signup).then(function (result) {
+                $scope.page.messages.push({
+                    contents: result.Message,
+                    type: result.Type
+                });
+                if (result.Type === 'success') {
+                    $window.location.href = "/Account/Login";
+                }
+            }, function (reason) {
+                $scope.page.messages.push({
+                    contents: reason,
+                    type: 'warning'
+                });
+            });
+        };
+    };
+    $scope.closeAlert = function (index) {
+        $scope.page.messages.splice(index, 1);
+    };
+})
+.controller('phoneNumber.signup', function ($scope, authorizeService, $window) {
+    $scope.action = "添加";
+    $scope.signup = {
+        phoneNumber: "",
+        code: "123"
+    };
+    $scope.verify = false;
+    $scope.signupForm = function () {
+        if ($scope.signup_form.$valid) {
+            if ($scope.verify) {
+                authorizeService.verifyPhoneNumber($scope.signup).then(function (result) {
+                    $scope.page.messages.push({
+                        contents: result,
+                        type: 'success'
+                    });
+                    $window.location.href = $scope.rootPath;
+                });
+            } else {
+                authorizeService.addPhoneNumber($scope.signup).then(function (result) {
+                    $scope.signup = {
+                        code: result.Code,
+                        phoneNumber: result.PhoneNumber
+                    };
+                    $scope.verify = true;
+                });
+            }
+        } else {
+            $scope.page.messages.push({
+                contents: '输入电话号码有误！请检查。',
+                type: 'warning'
+            });
+        }
+    };
+})
+.controller('phoneNumber.modify', function ($scope, authorizeService, $routeParams, $window) {
+    $scope.action = "修改";
+    $scope.signup = {
+        phoneNumber: $routeParams.number,
+        code: "123"
+    };
+    $scope.verify = false;
+    $scope.signupForm = function () {
+        if ($scope.signup_form.$valid) {
+            if ($scope.verify) {
+                authorizeService.verifyPhoneNumber($scope.signup).then(function (result) {
+                    $scope.page.messages.push({
+                        contents: result,
+                        type: 'success'
+                    });
+                    $window.location.href = $scope.rootPath;
+                });
+            } else {
+                authorizeService.addPhoneNumber($scope.signup).then(function (result) {
+                    $scope.signup = {
+                        code: result.Code,
+                        phoneNumber: result.PhoneNumber
+                    };
+                    $scope.verify = true;
+                });
+            }
+        } else {
+            $scope.page.messages.push({
+                contents: '输入电话号码有误！请检查。',
+                type: 'warning'
+            });
+        }
+    };
+});
--- LtePlatform/Scripts/manage/manage.role.user.js
+++
@@ -1,3 +0,0 @@
-﻿app.controller('manage.roleUser', function($scope, $routeParams, $http) {
-    $scope.panelTitle = $routeParams.name + "角色用户列表管理";
-});
