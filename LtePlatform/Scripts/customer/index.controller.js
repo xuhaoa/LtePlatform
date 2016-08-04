e93d2789e694d9f96654d698763cc81a5e4ff454
ouyh18 <ouyh18@189.cn>
2016-08-04 20:33:17 +0800

    2016-8-4 Refactor...

--- LtePlatform/Scripts/customer/index.controller.js
+++ LtePlatform/Scripts/customer/index.controller.js
@@ -202,4 +202,387 @@
         ];
 
         $scope.query();
+    })
+    .controller("complain.list", function($scope, complainService, customerQueryService) {
+        $scope.page.title = "10000号投诉";
+        $scope.query = function() {
+            complainService.queryAll($scope.beginDate.value, $scope.endDate.value).then(function(items) {
+                $scope.items = items;
+            });
+        };
+
+        customerQueryService.queryNetworkTypeOptions().then(function(options) {
+            $scope.type = {
+                options: options,
+                selected: options[0]
+            };
+        });
+        $scope.query();
+    })
+    .controller("complain.process", function($scope, $routeParams, customerQueryService, complainService) {
+
+        $scope.query = function() {
+            customerQueryService.queryOneComplain($routeParams.number).then(function(item) {
+                $scope.item = item;
+                if ($scope.item.nextStateDescription) {
+                    $scope.processInfo = "已完成" + $scope.item.nextStateDescription;
+                } else {
+                    $scope.processInfo = "";
+                }
+            });
+            complainService.queryComplainProcessList($routeParams.number).then(function(items) {
+                $scope.processItems = items;
+            });
+        };
+
+        $scope.createProcess = function() {
+            complainService.createComplainProcess($scope.item).then(function(process) {
+                if (process) {
+                    process.beginInfo = $scope.processInfo;
+                    complainService.updateComplainProcess(process).then(function() {
+                        $scope.query();
+                    });
+                }
+            });
+        };
+        $scope.save = function() {
+            complainService.updateComplain($scope.item).then(function() {
+                $scope.page.messages.push({
+                    type: 'success',
+                    contents: '完成10000号投诉工单：' + $scope.item.serialNumber
+                });
+            });
+        }
+
+        $scope.query();
+    })
+    .controller('complain.adjust', function($scope, complainService) {
+        $scope.page.title = "抱怨量信息校正";
+        $scope.items = [];
+
+        $scope.query = function() {
+            complainService.queryPositionList($scope.beginDate.value, $scope.endDate.value).then(function(list) {
+                $scope.items = list;
+            });
+        };
+
+        $scope.query();
+    })
+    .controller("complain.branch", function($scope, complainService, customerQueryService) {
+        $scope.page.title = "分公司投诉";
+        $scope.query = function() {
+            complainService.queryBranchList($scope.beginDate.value, $scope.endDate.value).then(function(items) {
+                $scope.items = items;
+            });
+        };
+
+        customerQueryService.queryNetworkTypeOptions().then(function(options) {
+            $scope.type = {
+                options: options,
+                selected: options[0]
+            };
+        });
+        $scope.query();
+    })
+    .controller("complain.online", function($scope, complainService, customerQueryService) {
+        $scope.page.title = "在线支撑";
+        $scope.query = function() {
+            complainService.queryOnlineList($scope.beginDate.value, $scope.endDate.value).then(function(items) {
+                $scope.items = items;
+            });
+        };
+
+        customerQueryService.queryNetworkTypeOptions().then(function(options) {
+            $scope.type = {
+                options: options,
+                selected: options[0]
+            };
+        });
+        $scope.query();
+    })
+    .controller("emergency.list", function($scope, customerDialogService, customerQueryService) {
+        $scope.page.title = "应急需求";
+        $scope.construct = function() {
+            customerDialogService.constructEmergencyCommunication(
+                $scope.city, $scope.district, $scope.type, $scope.page.messages, $scope.query);
+        };
+        $scope.query = function() {
+            customerQueryService.queryAll($scope.beginDate.value, $scope.endDate.value).then(function(items) {
+                $scope.items = items;
+            });
+        };
+
+        customerQueryService.queryVehicleTypeOptions().then(function(options) {
+            $scope.type = {
+                options: options,
+                selected: options[0]
+            };
+        });
+        $scope.query();
+    })
+    .controller("emergency.process", function($scope, $routeParams,
+        customerQueryService,
+        emergencyService,
+        customerDialogService) {
+
+        $scope.canGoNextStep = false;
+        $scope.fiberList = [];
+
+        $scope.query = function() {
+            customerQueryService.queryOneEmergency($routeParams.id).then(function(item) {
+                $scope.item = item;
+                if ($scope.item.nextStateDescription) {
+                    $scope.processInfo = "已完成" + $scope.item.nextStateDescription;
+                } else {
+                    $scope.processInfo = "";
+                }
+            });
+            emergencyService.queryProcessList($routeParams.id).then(function(list) {
+                $scope.processItems = list;
+            });
+        };
+
+        $scope.createProcess = function() {
+            emergencyService.createProcess($scope.item).then(function(process) {
+                if (process) {
+                    process.processInfo = $scope.processInfo;
+                    emergencyService.updateProcess(process).then(function() {
+                        $scope.query();
+                    });
+                }
+            });
+        };
+        $scope.queryFiberItems = function() {
+            emergencyService.queryFiberList($routeParams.id).then(function(list) {
+                $scope.fiberList = list;
+            });
+        };
+
+        $scope.createFiber = function() {
+            customerDialogService.constructFiberItem($routeParams.id, $scope.fiberList.length, function(item) {
+                $scope.canGoNextStep = true;
+                $scope.fiberList.push(item);
+            }, $scope.page.messages);
+        };
+
+        $scope.$watch('item.nextStateDescription', function(state) {
+            $scope.canGoNextStep = state && (state !== '光纤起单' || $scope.fiberList.length > 0);
+        });
+
+        $scope.query();
+        $scope.queryFiberItems();
+    })
+    .controller('emergency.new.dialog', function ($scope, $uibModalInstance, customerQueryService,
+        dialogTitle, city, district, vehicularType) {
+        $scope.dialogTitle = dialogTitle;
+        $scope.message = "";
+        $scope.city = city;
+        $scope.district = district;
+        $scope.vehicularType = vehicularType;
+
+        var firstDay = new Date();
+        firstDay.setDate(firstDay.getDate() + 7);
+        var nextDay = new Date();
+        nextDay.setDate(nextDay.getDate() + 14);
+        $scope.itemBeginDate = {
+            value: firstDay,
+            opened: false
+        };
+        $scope.itemEndDate = {
+            value: nextDay,
+            opened: false
+        };
+        customerQueryService.queryDemandLevelOptions().then(function (options) {
+            $scope.demandLevel = {
+                options: options,
+                selected: options[0]
+            };
+        });
+        var transmitOptions = customerQueryService.queryTransmitFunctionOptions();
+        $scope.transmitFunction = {
+            options: transmitOptions,
+            selected: transmitOptions[0]
+        };
+        var electrictOptions = customerQueryService.queryElectricSupplyOptions();
+        $scope.electricSupply = {
+            options: electrictOptions,
+            selected: electrictOptions[0]
+        };
+        $scope.dto = {
+            projectName: "和顺梦里水乡百合花文化节",
+            expectedPeople: 500000,
+            vehicles: 1,
+            area: "万顷洋园艺世界",
+            department: "南海区分公司客响维护部",
+            person: "刘文清",
+            phone: "13392293722",
+            vehicleLocation: "门口东边100米处",
+            otherDescription: "此次活动为佛山市南海区政府组织的一次大型文化活动，是宣传天翼品牌的重要场合。",
+            townId: 1
+        };
+
+        $scope.ok = function () {
+            $scope.dto.demandLevelDescription = $scope.demandLevel.selected;
+            $scope.dto.beginDate = $scope.itemBeginDate.value;
+            $scope.dto.endDate = $scope.itemEndDate.value;
+            $scope.dto.vehicularTypeDescription = $scope.vehicularType.selected;
+            $scope.dto.transmitFunction = $scope.transmitFunction.selected;
+            $scope.dto.district = $scope.district.selected;
+            $scope.dto.town = $scope.town.selected;
+            $scope.dto.electricSupply = $scope.electricSupply.selected;
+            $uibModalInstance.close($scope.dto);
+        };
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    })
+    .controller("vip.list", function ($scope, customerDialogService, customerQueryService) {
+        $scope.page.title = "政企支撑";
+        $scope.construct = function () {
+            customerDialogService.constructEmergencyCommunication(
+                $scope.city, $scope.district, $scope.type, $scope.page.messages, $scope.query);
+        };
+        $scope.query = function () {
+            customerQueryService.queryAllVip($scope.beginDate.value, $scope.endDate.value).then(function (items) {
+                $scope.items = items;
+            });
+        };
+
+        customerQueryService.queryNetworkTypeOptions().then(function (options) {
+            $scope.type = {
+                options: options,
+                selected: options[2]
+            };
+        });
+        $scope.query();
+    })
+    .controller('vip.supplement.dialog', function ($scope, $uibModalInstance,
+        customerQueryService, appFormatService,
+        dialogTitle, view, city, district) {
+        $scope.dialogTitle = dialogTitle;
+        $scope.view = view;
+        $scope.city = city;
+        $scope.district = district;
+        $scope.matchFunction = function (text) {
+            return $scope.view.projectName.indexOf(text) >= 0 || $scope.view.projectContents.indexOf(text) >= 0;
+        };
+        $scope.matchDistrictTown = function () {
+            var districtOption = appFormatService.searchText($scope.district.options, $scope.matchFunction);
+            if (districtOption) {
+                $scope.district.selected = districtOption;
+            }
+        };
+        $scope.$watch('town.selected', function () {
+            var townOption = appFormatService.searchText($scope.town.options, $scope.matchFunction);
+            if (townOption) {
+                $scope.town.selected = townOption;
+            }
+        });
+
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+        $scope.ok = function () {
+            $scope.view.district = $scope.district.selected;
+            $scope.view.town = $scope.town.selected;
+            $uibModalInstance.close($scope.view);
+        };
+    })
+    .controller("vip.process", function ($scope, $routeParams, customerQueryService, emergencyService) {
+
+        $scope.query = function () {
+            customerQueryService.queryOneVip($routeParams.number).then(function (item) {
+                $scope.item = item;
+                if ($scope.item.nextStateDescription) {
+                    $scope.processInfo = "已完成" + $scope.item.nextStateDescription;
+                } else {
+                    $scope.processInfo = "";
+                }
+                customerQueryService.queryMarketThemeOptions().then(function (options) {
+                    $scope.marketTheme = {
+                        options: options,
+                        selected: item.marketThemeDescription
+                    };
+                });
+            });
+            emergencyService.queryVipProcessList($routeParams.number).then(function (items) {
+                $scope.processItems = items;
+            });
+        };
+
+        $scope.createProcess = function () {
+            emergencyService.createVipProcess($scope.item).then(function (process) {
+                if (process) {
+                    process.beginInfo = $scope.processInfo;
+                    emergencyService.updateVipProcess(process).then(function () {
+                        $scope.query();
+                    });
+                }
+            });
+        };
+        $scope.save = function () {
+            $scope.item.marketThemeDescription = $scope.marketTheme.selected;
+            customerQueryService.updateVip($scope.item).then(function () {
+                $scope.page.messages.push({
+                    type: 'success',
+                    contents: '完成政企需求工单：' + $scope.item.serialNumber
+                });
+            });
+        }
+
+        $scope.query();
+    })
+    .controller('complain.supplement.dialog', function ($scope, $uibModalInstance,
+        appRegionService, appFormatService, baiduMapService, parametersMapService, parametersDialogService, item) {
+        $scope.dialogTitle = item.serialNumber + "工单信息补充";
+
+        $scope.item = item;
+        appRegionService.initializeCities().then(function (cities) {
+            $scope.city.options = cities;
+            $scope.city.selected = cities[0];
+            appRegionService.queryDistricts($scope.city.selected).then(function (districts) {
+                $scope.district.options = districts;
+                $scope.district.selected = (item.district) ? item.district.replace('区', '') : districts[0];
+                baiduMapService.initializeMap("map", 11);
+                baiduMapService.addCityBoundary("佛山");
+                if (item.longtitute && item.lattitute) {
+                    var marker = baiduMapService.generateMarker(item.longtitute, item.lattitute);
+                    baiduMapService.addOneMarker(marker);
+                    baiduMapService.setCellFocus(item.longtitute, item.lattitute, 15);
+                }
+                if (item.sitePosition) {
+                    parametersMapService.showElementsWithGeneralName(item.sitePosition,
+                        parametersDialogService.showENodebInfo, parametersDialogService.showCellInfo);
+                }
+            });
+        });
+
+        $scope.matchTown = function () {
+            var town = appFormatService.searchPattern($scope.town.options, item.sitePosition);
+            if (town) {
+                $scope.town.selected = town;
+                return;
+            }
+            town = appFormatService.searchPattern($scope.town.options, item.buildingName);
+            if (town) {
+                $scope.town.selected = town;
+                return;
+            }
+            town = appFormatService.searchPattern($scope.town.options, item.roadName);
+            if (town) {
+                $scope.town.selected = town;
+            }
+        };
+
+        $scope.ok = function () {
+            $scope.item.district = $scope.district.selected;
+            $scope.item.town = $scope.town.selected;
+            $uibModalInstance.close($scope.item);
+        };
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    })
+    .controller("daily.stat", function ($scope) {
+        $scope.page.title = "客服指标统计分析";
     });
