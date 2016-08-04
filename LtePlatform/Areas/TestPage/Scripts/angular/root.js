56b3b972e16d3e45fb42c0cba7d3481f1ef436b6
ouyh18 <ouyh18@189.cn>
2016-08-04 09:05:40 +0800

    2016-8-4 Refactor...

---
+++ LtePlatform/Areas/TestPage/Scripts/angular/root.js
@@ -0,0 +1,86 @@
+﻿angular.module('myApp', ['test.angular.root', 'test.angular.index']);
+
+angular.module('test.angular.index', ['app.common'])
+    .controller("root.property", function($scope) {
+        $scope.pageTitle = "RootProperty";
+    })
+    .controller('ParentController', function($scope) {
+        // 使用.controller访问`ng-controller`内部的属性
+        // 在DOM忽略的$scope中，根据当前控制器进行推断
+        $scope.parentProperty = 'parent scope';
+    })
+    .controller('ChildController', function($scope) {
+        $scope.childProperty = 'child scope';
+        // 同在DOM中一样，我们可以通过当前$scope直接访问原型中的任意属性
+        $scope.fullSentenceFromChild = 'Same $scope: We can access: ' +
+            $scope.rootProperty + ' and ' +
+            $scope.parentProperty + ' and ' +
+            $scope.childProperty;
+    })
+    .directive('myDirective', function() {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                myUrl: '@', //绑定策略
+                myLinkText: '@' //绑定策略
+            },
+            template: '<a href="{{myUrl}}">' +
+                '{{myLinkText}}</a>'
+        };
+    })
+    .directive('theirDirective', function() {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                myUrl: '=someAttr', // 经过了修改
+                myLinkText: '@'
+            },
+            template: '\
+            <div>\
+                <label>My Url Field:</label>\
+                <input type="text"\
+                    ng-model="myUrl" />\
+                <a href="{{myUrl}}">{{myLinkText}}</a>\
+            </div>'
+        };
+    }).directive('ngFocus', [
+        function() {
+            var FOCUS_CLASS = "ng-focused";
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function(scope, element, attrs, ctrl) {
+                    ctrl.$focused = false;
+                    element.bind('focus', function(evt) {
+                        element.addClass(FOCUS_CLASS);
+                        scope.$apply(function() {
+                            ctrl.$focused = true;
+                        });
+                    }).bind('blur', function(evt) {
+                        element.removeClass(FOCUS_CLASS);
+                        scope.$apply(function() {
+                            ctrl.$focused = false;
+                        });
+                    });
+                }
+            };
+        }
+    ])
+    .controller("submit.form", function($scope) {
+        $scope.pageTitle = "SubmitForm";
+    })
+    .controller('signupController', [
+        '$scope', function($scope) {
+            $scope.submitted = false;
+            $scope.signupForm = function() {
+                if ($scope.signup_form.$valid) {
+                    // Submit as normal
+                    alert("The form is submit!");
+                } else {
+                    $scope.signup_form.submitted = true;
+                }
+            };
+        }
+    ]);
--- LtePlatform/Areas/TestPage/Scripts/angular/route.js
+++ LtePlatform/Areas/TestPage/Scripts/angular/route.js
@@ -1,21 +1,91 @@
-﻿app.config([
-    '$routeProvider', function($routeProvider) {
-        var viewDir = "/appViews/Test/Angular/";
-        $routeProvider
-            .when('/', {
-                templateUrl: viewDir + "SubmitForm.html",
-                controller: "submit.form"
-            })
-            .when('/root', {
-                templateUrl: viewDir + "RootProperty.html",
-                controller: "root.property"
-            })
-            .when('/legacy', {
-                templateUrl: viewDir + "LegacyMarkup.html",
-                controller: "legacy.markup"
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }
-]);
+﻿angular.module('test.angular.root', ['app.common'])
+    .run(function($rootScope, $timeout) { // 使用.run访问$rootScope
+        $timeout(function() {
+            $rootScope.myHref = 'http://google.com';
+        }, 2000);
+        $rootScope.rootProperty = 'root scope';
+        var rootPath = '/TestPage/AngularTest/';
+        var indexPath = '/TestPage/AngularTest#/';
+        $rootScope.menuItems = [
+            {
+                displayName: "AngularJS测试",
+                isActive: true,
+                subItems: [
+                    {
+                        name: "Simple",
+                        displayName: "Simple Type Test",
+                        url: rootPath + 'Simple'
+                    }, {
+                        name: "SubmitForm",
+                        displayName: "Submit Form",
+                        url: indexPath
+                    }, {
+                        name: "RootProperty",
+                        displayName: "Root Property",
+                        url: indexPath + "root"
+                    }, {
+                        name: "Chapter9Ari",
+                        displayName: "第 9 章　内置指令",
+                        url: rootPath + "Chapter9Ari"
+                    }, {
+                        name: "Chapter10Ari",
+                        displayName: "第 10 章　指令详解",
+                        url: rootPath + "Chapter10Ari"
+                    }
+                ]
+            }, {
+                displayName: "CoffeeScript脚本测试",
+                isActive: false,
+                subItems: [
+                    {
+                        displayName: "Hotseat 5x5",
+                        url: "/TestPage/CoffeeScript/Hotseat",
+                        tooltip: "全网LTE和CDMA基站、小区列表和地理化显示、对全网的基站按照基站名称、地址等信息进行查询，并进行个别基站小区的增删、修改信息的操作"
+                    }
+                ]
+            }, {
+                displayName: "WebApi测试",
+                isActive: false,
+                subItems: [
+                    {
+                        displayName: "工参上传测试",
+                        url: "/TestPage/WebApiTest/BasicPost",
+                        tooltip: "全网LTE和CDMA基站、小区列表和地理化显示、对全网的基站按照基站名称、地址等信息进行查询，并进行个别基站小区的增删、修改信息的操作"
+                    }, {
+                        displayName: "简单类型测试",
+                        url: "/TestPage/WebApiTest/SimpleType",
+                        tooltip: "对传统指标（主要是2G和3G）的监控、分析和地理化呈现"
+                    }, {
+                        displayName: "Html5csv测试",
+                        url: "/TestPage/WebApiTest/Html5Test",
+                        tooltip: "对接本部优化部4G网优平台，实现对日常工单的监控和分析"
+                    }, {
+                        displayName: "Html5csv测试-2",
+                        url: "/TestPage/WebApiTest/Html5PostTest",
+                        tooltip: "校园网专项优化，包括数据管理、指标分析、支撑工作管理和校园网覆盖呈现"
+                    }
+                ]
+            }
+        ];
+    })
+    .config([
+        '$routeProvider', function($routeProvider) {
+            var viewDir = "/appViews/Test/Angular/";
+            $routeProvider
+                .when('/', {
+                    templateUrl: viewDir + "SubmitForm.html",
+                    controller: "submit.form"
+                })
+                .when('/root', {
+                    templateUrl: viewDir + "RootProperty.html",
+                    controller: "root.property"
+                })
+                .when('/legacy', {
+                    templateUrl: viewDir + "LegacyMarkup.html",
+                    controller: "legacy.markup"
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    ]);
