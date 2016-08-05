58f5a94762db53c932d320aa145c74b0155368df
ouyh18 <ouyh18@189.cn>
2016-08-05 18:31:52 +0800

    2016-8-5 Add some ui-grid tests.

---
+++ LtePlatform/Scripts/test/ui-grid/gridObjectTestUtils.spec.js
@@ -0,0 +1,31 @@
+﻿/* globals element, by */
+
+var gridTestUtils = require('./gridTestUtils.spec.js');
+
+function getProxyToRealMethod(gridId, method) {
+    return function () {
+        var callArgs = [gridId].concat(Array.prototype.slice.call(arguments));
+        return gridTestUtils[method].apply(gridTestUtils, callArgs);
+    };
+}
+
+/**
+ * @ngdoc overview
+ * @name ui.grid.e2eTestLibrary
+ * @description
+ * End to end test functions.
+ */
+
+/**
+ * @ngdoc service
+ * @name ui.grid.e2eTestLibrary.api:gridTestObject
+ * @description
+ * End to end test functions.
+ */
+module.exports = function (gridId) {
+    for (var method in gridTestUtils) {
+        if (gridTestUtils.hasOwnProperty(method)) {
+            this[method] = getProxyToRealMethod(gridId, method);
+        }
+    }
+};
