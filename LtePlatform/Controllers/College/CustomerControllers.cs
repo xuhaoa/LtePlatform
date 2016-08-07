3675370ae993e6520ff652b2d372b9af2766bc22
ouyh18 <ouyh18@189.cn>
2016-08-07 14:14:00 +0800

    2016-8-7 Refactor...

--- LtePlatform/Controllers/College/CustomerControllers.cs
+++ LtePlatform/Controllers/College/CustomerControllers.cs
@@ -280,6 +280,7 @@ namespace LtePlatform.Controllers.College
         }
     }
 
+    [ApiControl("分公司需求查询控制器")]
     public class BranchDemandController : ApiController
     {
         private readonly BranchDemandService _service;
@@ -290,12 +291,18 @@ namespace LtePlatform.Controllers.College
         }
 
         [HttpGet]
+        [ApiDoc("查询到指定日期当月发生的分公司需求数")]
+        [ApiParameterDoc("today", "指定统计日期")]
         public async Task<int> GetCount(DateTime today)
         {
             return await _service.QueryCount<BranchDemandService, BranchDemand>(today);
         }
 
         [HttpGet]
+        [ApiDoc("查询指定日期范围内的分公司需求列表")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("分公司需求列表")]
         public List<BranchDemandDto> Get(DateTime begin, DateTime end)
         {
             return _service.QueryList(begin, end);
