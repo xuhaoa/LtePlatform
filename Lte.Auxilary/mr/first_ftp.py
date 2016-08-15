fa089f22a3dee2ad9146ae540880518ebf049b97
ouyh18 <ouyh18@189.cn>
2016-08-15 09:49:56 +0800

    2016-8-15 Add some python files.

--- Lte.Auxilary/mr/first_ftp.py
+++ Lte.Auxilary/mr/first_ftp.py
@@ -1,23 +1,41 @@
 import ftputil
 import os
 import shutil
+import pymongo
+from pymongo import MongoClient
+
+# 从数据库获取已下载列表
+db = MongoClient('mongodb://root:Abcdef9*@10.17.165.106')['ouyh']
+_DFlist = list(db['DFlist'].find({}, {'dfName': 1, '_id': 0}))      # downloadfiles已下载的文件列表
+DFList = [item.get('dfName') for item in _DFlist]
+#print(DFList)
 
 host = ftputil.FTPHost('132.122.152.115', 'ouyh18', 'O123#')
-if os.path.isdir('temp'):
-    shutil.rmtree('temp')
-
-os.mkdir('temp')
+if not os.path.isdir('temp'):
+    os.mkdir('temp')
 os.chdir('temp')
 
 _tmpfolder = []
 _tmpfile = []
 
-for root, dirs, files in host.walk('/ZIP_MR_HW_SOURCE_D'):
-    print(root)
+for root, dirs, files in host.walk('/MR_HW_SOURCE_D/20160815/201608150215'):
+    print('Root path: ' + root)
+    print('Directories: ')
     print(dirs)
+    print('Files: ')
     print(files)
-
     host.chdir(root)
     for name in files:
-        print(os.path.join(root, name))
-        host.download(name, name)
+        print('File: ' + os.path.join(root, name))
+        # 判断文件是否已经存在于数据库
+        if name in DFList:
+            pass
+        else:
+            try:
+                host.download(name, name)
+            except:
+                pass
+            DFList.append(name)
+            db['DFlist'].insert({'dfName': name})
+            print('Download finished: ' + os.path.join(root, name))
+        
--- Lte.Auxilary/mr/huawei_test_ftp.py
+++ Lte.Auxilary/mr/huawei_test_ftp.py
@@ -1,7 +1,9 @@
 import ftputil
+import shutil
+import os
 
 HOST_HW = ['132.122.152.115', '132.122.152.112', '132.122.152.124'] 
-FOLDER_HW = ['/ZIP_MR_HW_SOURCE_D', '/ZIP_MR_HW_SOURCE_E']
+FOLDER_HW = ['/MR_HW_SOURCE_D', '/MR_HW_SOURCE_E']
 
 for host_ip in HOST_HW:
     for folder in FOLDER_HW:
@@ -10,7 +12,7 @@ for host_ip in HOST_HW:
             try:
                 host = ftputil.FTPHost(host_ip, 'ouyh18', 'O123#')
                 for root, dirs, files in host.walk(folder):
-                    if root.count('/') < 3 and not root.endswith(tuple(DDlist)) and not root.endswith(date.today().strftime('%Y%m%d')):
+                    if root.count('/') < 3 and root.endswith(date.today().strftime('%Y%m%d')):
                         print(len(files))
                         print(files)
 
