490150e986f537729e124890ffd57a0f7ad3d3a9
ouyh18 <ouyh18@189.cn>
2016-08-25 17:31:31 +0800

    2016-8-25 Refactor...

--- LtePlatform/Scripts/college/route.controller.js
+++ LtePlatform/Scripts/college/route.controller.js
@@ -985,10 +985,55 @@
             $uibModalInstance.dismiss('cancel');
         };
     })
-    .controller('college.test3G.dialog', function ($scope, $uibModalInstance, collegeName, collegeDtService) {
+    .controller('college.test3G.dialog', function ($scope, $uibModalInstance, collegeName,
+        collegeDtService, coverageService, collegeMapService, geometryService) {
         $scope.dialogTitle = collegeName + "-3G测试结果上报";
         $scope.item = collegeDtService.default3GTestView(collegeName, '饭堂', '许良镇');
 
+        var queryRasterInfo = function (files, index, data, callback) {
+            coverageService.queryDetailsByRasterInfo(files[index], '3G').then(function (result) {
+                data.push.apply(data, result);
+                if (index < files.length - 1) {
+                    queryRasterInfo(files, index + 1, data, callback);
+                } else {
+                    callback(data);
+                }
+            });
+        };
+        collegeMapService.queryCenterAndCallback(collegeName, function (center) {
+            geometryService.transformToBaidu(center.X, center.Y).then(function (coors) {
+                $scope.center = {
+                    centerX: 2 * center.X - coors.x,
+                    centerY: 2 * center.Y - coors.y
+                };
+            });
+        });
+
+        $scope.matchCoverage = function () {
+            collegeDtService.queryRaster($scope.center, '3G', $scope.beginDate.value, $scope.endDate.value, function (files) {
+                if (files.length) {
+                    var data = [];
+                    queryRasterInfo(files, 0, data, function (result) {
+                        console.log(result);
+                        //var testEvaluations = appFormatService.calculateAverages(result, [
+                        //    function (point) {
+                        //        return point.rsrp;
+                        //    }, function (point) {
+                        //        return point.sinr;
+                        //    }, function (point) {
+                        //        return point.pdcpThroughputDl > 10 ? point.pdcpThroughputDl : 0;
+                        //    }, function (point) {
+                        //        return point.pdcpThroughputUl > 1 ? point.pdcpThroughputUl : 0;
+                        //    }
+                        //]);
+                        //$scope.item.rsrp = testEvaluations[0].sum / testEvaluations[0].count;
+                        //$scope.item.sinr = testEvaluations[1].sum / testEvaluations[1].count;
+                        //$scope.item.downloadRate = testEvaluations[2].sum / testEvaluations[2].count * 1024;
+                        //$scope.item.uploadRate = testEvaluations[3].sum / testEvaluations[3].count * 1024;
+                    });
+                }
+            });
+        };
         $scope.ok = function () {
             $uibModalInstance.close($scope.item);
         };
@@ -1054,7 +1099,10 @@
                                 return point.pdcpThroughputUl > 1 ? point.pdcpThroughputUl : 0;
                             }
                         ]);
-                        console.log(testEvaluations);
+                        $scope.item.rsrp = testEvaluations[0].sum / testEvaluations[0].count;
+                        $scope.item.sinr = testEvaluations[1].sum / testEvaluations[1].count;
+                        $scope.item.downloadRate = testEvaluations[2].sum / testEvaluations[2].count * 1024;
+                        $scope.item.uploadRate = testEvaluations[3].sum / testEvaluations[3].count * 1024;
                     });
                 }
             });
