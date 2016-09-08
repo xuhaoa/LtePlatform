c0d0b0883c20e9c6e6326d8082d2d77d8633b0e8
ouyh18 <ouyh18@189.cn>
2016-09-08 11:34:00 +0800

    2016-9-8 Refactor...

--- Lte.MySqlFramework/Entities/FlowHuawei.cs
+++ Lte.MySqlFramework/Entities/FlowHuawei.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Abp.Domain.Entities;
 using Abp.EntityFramework.AutoMapper;
 using Lte.Domain.Common;
+using Lte.Domain.LinqToCsv;
+using Lte.Domain.LinqToCsv.Context;
+using Lte.Domain.LinqToCsv.Description;
+using Lte.Domain.Regular;
 
 namespace Lte.MySqlFramework.Entities
 {
@@ -83,5 +90,133 @@ namespace Lte.MySqlFramework.Entities
 
         [AutoMapPropertyResolve("SchedulingRank2String", typeof(FlowHuaweiCsv), typeof(StringToIntTransform))]
         public int SchedulingRank2 { get; set; }
+    }
+
+    public class FlowHuaweiCsv
+    {
+        [CsvColumn(Name = "开始时间")]
+        public DateTime StatTime { get; set; }
+
+        [CsvColumn(Name = "小区")]
+        public string CellInfo { get; set; }
+
+        public int ENodebId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(CellInfo)) return 0;
+                var fields = CellInfo.GetSplittedFields(", ");
+                return fields.Length == 0 ? 0 : fields[3].GetSplittedFields('=')[1].ConvertToInt(0);
+            }
+        }
+
+        public byte LocalCellId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(CellInfo)) return 0;
+                var fields = CellInfo.GetSplittedFields(", ");
+                return fields.Length == 0 ? (byte)0 : fields[1].GetSplittedFields('=')[1].ConvertToByte(0);
+            }
+        }
+
+        [CsvColumn(Name = "小区PDCP层所发送的下行数据的总吞吐量 (比特)")]
+        public long PdcpDownlinkFlowInByte { get; set; }
+
+        [CsvColumn(Name = "小区PDCP层所接收到的上行数据的总吞吐量 (比特)")]
+        public long PdcpUplinkFlowInByte { get; set; }
+
+        [CsvColumn(Name = "小区内的平均用户数 (无)")]
+        public double AverageUsers { get; set; }
+
+        [CsvColumn(Name = "小区内的最大用户数 (无)")]
+        public int MaxUsers { get; set; }
+
+        [CsvColumn(Name = "平均激活用户数 (无)")]
+        public double AverageActiveUsers { get; set; }
+
+        [CsvColumn(Name = "最大激活用户数 (无)")]
+        public int MaxActiveUsers { get; set; }
+
+        [CsvColumn(Name = "上行平均激活用户数 (无)")]
+        public double UplinkAverageUsers { get; set; }
+
+        [CsvColumn(Name = "上行最大激活用户数 (无)")]
+        public int UplinkMaxUsers { get; set; }
+
+        [CsvColumn(Name = "下行平均激活用户数 (无)")]
+        public double DownlinkAverageUsers { get; set; }
+
+        [CsvColumn(Name = "下行最大激活用户数 (无)")]
+        public int DownlinkMaxUsers { get; set; }
+
+        [CsvColumn(Name = "小区下行有数据传输总时长(1ms精度) (毫秒)")]
+        public int DownlinkDurationInMs { get; set; }
+
+        [CsvColumn(Name = "小区上行有数据传输总时长(1ms精度) (毫秒)")]
+        public int UplinkDurationInMs { get; set; }
+
+        [CsvColumn(Name = "小区Uu接口寻呼用户个数 (无)")]
+        public string PagingUsersString { get; set; }
+
+        [CsvColumn(Name = "下行Physical Resource Block被使用的平均个数 (无)")]
+        public double DownlinkAveragePrbs { get; set; }
+
+        [CsvColumn(Name = "下行PDSCH DRB的Physical Resource Block被使用的平均个数 (无)")]
+        public double DownlinkDrbPbs { get; set; }
+
+        [CsvColumn(Name = "上行Physical Resource Block被使用的平均个数 (无)")]
+        public double UplinkAveragePrbs { get; set; }
+
+        [CsvColumn(Name = "上行PUSCH DRB的Physical Resource Block被使用的平均个数 (无)")]
+        public double UplinkDrbPbs { get; set; }
+
+        [CsvColumn(Name = "小区接收到属于Group A的Preamble消息次数 (无)")]
+        public int GroupAPreambles { get; set; }
+
+        [CsvColumn(Name = "小区接收到属于Group B的Preamble消息的次数 (无)")]
+        public int GroupBPreambles { get; set; }
+
+        [CsvColumn(Name = "小区接收到专用前导消息的次数 (无)")]
+        public int DedicatedPreambles { get; set; }
+
+        [CsvColumn(Name = "统计周期内上行DCI所使用的PDCCH CCE个数 (无)")]
+        public long UplinkDciCces { get; set; }
+
+        public double UplinkDciCceRate => TotalCces == 0 ? 0 : (double)UplinkDciCces / TotalCces;
+
+        [CsvColumn(Name = "统计周期内下行DCI所使用的PDCCH CCE个数 (无)")]
+        public long DownlinkDciCces { get; set; }
+
+        public double DownlinkDciCceRate => TotalCces == 0 ? 0 : (double)DownlinkDciCces / TotalCces;
+
+        [CsvColumn(Name = "统计周期内可用的PDCCH CCE的个数 (无)")]
+        public long TotalCces { get; set; }
+
+        [CsvColumn(Name = "PUCCH的PRB资源分配的平均值 (无)")]
+        public string PucchPrbsString { get; set; }
+
+        [CsvColumn(Name = "使UE缓存为空的最后一个TTI所传的上行PDCP吞吐量 (比特)")]
+        public long LastTtiUplinkFlowInByte { get; set; }
+
+        [CsvColumn(Name = "扣除使UE缓存为空的最后一个TTI之后的上行数传时长 (毫秒)")]
+        public int ButLastUplinkDurationInMs { get; set; }
+
+        [CsvColumn(Name = "使缓存为空的最后一个TTI所传的下行PDCP吞吐量 (比特)")]
+        public long LastTtiDownlinkFlowInByte { get; set; }
+
+        [CsvColumn(Name = "扣除使下行缓存为空的最后一个TTI之后的数传时长 (毫秒)")]
+        public int ButLastDownlinkDurationInMs { get; set; }
+
+        [CsvColumn(Name = "RANK1的上报次数 (无)")]
+        public string SchedulingRank1String { get; set; }
+
+        [CsvColumn(Name = "RANK2的上报次数 (无)")]
+        public string SchedulingRank2String { get; set; }
+
+        public static List<FlowHuaweiCsv> ReadFlowHuaweiCsvs(StreamReader reader)
+        {
+            return CsvContext.Read<FlowHuaweiCsv>(reader, CsvFileDescription.CommaDescription).ToList();
+        }
     }
 }
---
+++ Lte.MySqlFramework/Entities/PlanningSite.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.EntityFramework.AutoMapper;
+using Lte.Domain.Common.Geo;
+using Lte.Domain.Regular.Attributes;
+
+namespace Lte.MySqlFramework.Entities
+{
+    [AutoMapFrom(typeof(PlanningSiteExcel))]
+    public class PlanningSite : Entity, ITownId
+    {
+        public int TownId { get; set; }
+
+        public string PlanNum { get; set; }
+        
+        public string PlanName { get; set; }
+
+        public string FormalName { get; set; }
+
+        public double Longtitute { get; set; }
+
+        public double Lattitute { get; set; }
+
+        public string TowerType { get; set; }
+        
+        public double? AntennaHeight { get; set; }
+        
+        public DateTime? GottenDate { get; set; }
+        
+        public DateTime? ContractDate { get; set; }
+        
+        public DateTime? FinishedDate { get; set; }
+    }
+
+    public class PlanningSiteExcel : IDistrictTown
+    {
+        [ExcelColumn("区县")]
+        public string District { get; set; }
+
+        [ExcelColumn("分局")]
+        public string Town { get; set; }
+
+        [ExcelColumn("编号")]
+        public string PlanNum { get; set; }
+
+        [ExcelColumn("规划名")]
+        public string PlanName { get; set; }
+
+        [ExcelColumn("电信出图站名")]
+        public string FormalName { get; set; }
+
+        [ExcelColumn("规划经度")]
+        public double PlanLongtitute { get; set; }
+
+        [ExcelColumn("规划纬度")]
+        public double PlanLattitute { get; set; }
+
+        [ExcelColumn("选址经度")]
+        public double? FinalLongtitute { get; set; }
+
+        [ExcelColumn("选址纬度")]
+        public double? FinalLattitute { get; set; }
+
+        public double Longtitute => FinalLongtitute ?? PlanLongtitute;
+
+        public double Lattitute => FinalLattitute ?? PlanLattitute;
+
+        [ExcelColumn("杆塔类型")]
+        public string TowerType { get; set; }
+
+        [ExcelColumn("天线挂高")]
+        public double? AntennaHeight { get; set; }
+
+        [ExcelColumn("谈点完成日期")]
+        public DateTime? GottenDate { get; set; }
+
+        [ExcelColumn("合同签订日期")]
+        public DateTime? ContractDate { get; set; }
+
+        [ExcelColumn("开通日期")]
+        public DateTime? FinishedDate { get; set; }
+    }
+}
