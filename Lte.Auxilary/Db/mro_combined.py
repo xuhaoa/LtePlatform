7c6f962a1f60e14f3a0db52b061c0a360d3b4171
ouyh18 <ouyh18@189.cn>
2016-09-19 17:31:35 +0800

    2016-9-19 Refactor...

--- Lte.Auxilary/Db/mro_combined.py
+++ Lte.Auxilary/Db/mro_combined.py
@@ -12,11 +12,13 @@ time=time.replace(hour=0,minute=0,second=0)
 datestr=time.strftime("%Y%m%d")
 
 for pci in range(504):
-    Pci_List=list(db['mro_'+datestr].find({'Pci': pci}))
-    df = DataFrame(Pci_List)
-    stat=df.groupby(['CellId','Pci','NeighborPci']).sum().reset_index()
-    statList = json.loads(stat.T.to_json()).values()
-    for item in statList:
-        item.update({'StatDate': time})
-    db['mro_combined'].insert_many(statList)
-    print('Pci: ', pci, ' inserted items: ', len(statList))
+    for nPci in range(504):
+        Pci_List=list(db['mro_'+datestr].find({'Pci': pci, 'NeighborPci': nPci}))
+        if len(Pci_List)>0:
+            df = DataFrame(Pci_List)
+            stat=df.groupby(['CellId','Pci','NeighborPci']).sum().reset_index()
+            statList = json.loads(stat.T.to_json()).values()
+            for item in statList:
+                item.update({'StatDate': time})
+            db['mro_combined'].insert_many(statList)
+            print('Pci: ', pci, ' inserted items: ', len(statList))
---
+++ Lte.Auxilary/mr/process_zte_mrs_30.sh
@@ -0,0 +1,1 @@
+/home/wireless/env/py3env/bin/python /home/wireless/Public/process_zte_mrs_30.py
