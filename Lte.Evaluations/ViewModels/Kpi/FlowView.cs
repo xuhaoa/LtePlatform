10c823a686424bd1c7f1e1d05731caccec0dcfcf
ouyh18 <ouyh18@189.cn>
2016-11-30 19:54:41 +0800

    2016-11-30 Refactor...

--- Lte.Evaluations/ViewModels/Kpi/FlowView.cs
+++ Lte.Evaluations/ViewModels/Kpi/FlowView.cs
@@ -5,33 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using Abp.EntityFramework.AutoMapper;
 using Lte.Domain.Common;
+using Lte.Domain.Regular.Attributes;
 using Lte.MySqlFramework.Entities;
 
 namespace Lte.Evaluations.ViewModels.Kpi
 {
     [AutoMapFrom(typeof(FlowHuawei), typeof(FlowZte))]
+    [TypeDoc("小区单日流量统计视图")]
     public class FlowView
     {
+        [MemberDoc("统计时间")]
         public DateTime StatTime { get; set; }
 
+        [MemberDoc("基站编号")]
         public int ENodebId { get; set; }
 
+        [MemberDoc("扇区编号")]
         public byte SectorId { get; set; }
 
         [AutoMapPropertyResolve("DownlinkPdcpFlow", typeof(FlowZte))]
+        [MemberDoc("PDCP层下行流量")]
         public double PdcpDownlinkFlow { get; set; }
 
         [AutoMapPropertyResolve("UplindPdcpFlow", typeof(FlowZte))]
+        [MemberDoc("PDCP层上行流量")]
         public double PdcpUplinkFlow { get; set; }
 
         [AutoMapPropertyResolve("AverageRrcUsers", typeof(FlowZte))]
+        [MemberDoc("平均用户数")]
         public double AverageUsers { get; set; }
 
         [AutoMapPropertyResolve("MaxRrcUsers", typeof(FlowZte))]
+        [MemberDoc("最大用户数")]
         public int MaxUsers { get; set; }
 
+        [MemberDoc("平均激活用户数")]
         public double AverageActiveUsers { get; set; }
 
+        [MemberDoc("最大激活用户数")]
         public int MaxActiveUsers { get; set; }
 
         public static FlowView Average(IEnumerable<FlowView> views)
@@ -54,20 +65,28 @@ namespace Lte.Evaluations.ViewModels.Kpi
     }
 
     [AutoMapFrom(typeof(FlowView))]
+    [TypeDoc("聚合流量统计视图")]
     public class AggregateFlowView
     {
+        [MemberDoc("小区个数")]
         public int CellCount { get; set; }
 
+        [MemberDoc("PDCP层下行流量")]
         public double PdcpDownlinkFlow { get; set; }
-        
+
+        [MemberDoc("PDCP层上行流量")]
         public double PdcpUplinkFlow { get; set; }
-        
+
+        [MemberDoc("平均用户数")]
         public double AverageUsers { get; set; }
-        
+
+        [MemberDoc("最大用户数")]
         public int MaxUsers { get; set; }
 
+        [MemberDoc("平均激活用户数")]
         public double AverageActiveUsers { get; set; }
 
+        [MemberDoc("最大激活用户数")]
         public int MaxActiveUsers { get; set; }
     }
 }
--- LtePlatform/Controllers/Kpi/FlowQueryController.cs
+++ LtePlatform/Controllers/Kpi/FlowQueryController.cs
@@ -10,9 +10,11 @@ using Lte.Evaluations.DataService.College;
 using Lte.Evaluations.DataService.Kpi;
 using Lte.Evaluations.ViewModels.Kpi;
 using Lte.MySqlFramework.Entities;
+using LtePlatform.Models;
 
 namespace LtePlatform.Controllers.Kpi
 {
+    [ApiControl("流量查询控制器")]
     public class FlowQueryController : ApiController
     {
         private readonly FlowQueryService _service;
@@ -23,18 +25,31 @@ namespace LtePlatform.Controllers.Kpi
         }
 
         [HttpGet]
+        [ApiDoc("查询指定日期范围内指定小区流量情况，按照日期排列")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiParameterDoc("sectorId", "扇区编号")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("流量情况，按照日期排列")]
         public List<FlowView> Get(int eNodebId, byte sectorId, DateTime begin, DateTime end)
         {
             return _service.QueryFlow(eNodebId, sectorId, begin.Date, end.Date);
         }
 
         [HttpGet]
+        [ApiDoc("查询指定日期范围内指定小区平均流量情况")]
+        [ApiParameterDoc("eNodebId", "基站编号")]
+        [ApiParameterDoc("sectorId", "扇区编号")]
+        [ApiParameterDoc("beginDate", "开始日期")]
+        [ApiParameterDoc("endDate", "结束日期")]
+        [ApiResponse("平均流量情况")]
         public FlowView GetAverage(int eNodebId, byte sectorId, DateTime beginDate, DateTime endDate)
         {
             return _service.QueryAverageView(eNodebId, sectorId, beginDate, endDate);
         }
     }
 
+    [ApiControl("镇级流量查询控制器")]
     public class TownFlowController : ApiController
     {
         private readonly TownFlowService _service;
@@ -45,12 +60,16 @@ namespace LtePlatform.Controllers.Kpi
         }
 
         [HttpGet]
+        [ApiDoc("查询指定日期前镇级天流量")]
+        [ApiParameterDoc("statDate", "统计日期")]
+        [ApiResponse("镇级天流量，每个镇一条记录")]
         public IEnumerable<TownFlowView> Get(DateTime statDate)
         {
             return _service.QueryLastDateStat(statDate);
         } 
     }
 
+    [ApiControl("校园网流量查询控制器")]
     public class CollegeFlowController : ApiController
     {
         private readonly FlowQueryService _service;
@@ -63,6 +82,11 @@ namespace LtePlatform.Controllers.Kpi
         }
 
         [HttpGet]
+        [ApiDoc("查询指定学校指定日期范围内流量情况")]
+        [ApiParameterDoc("collegeName", "学校名称")]
+        [ApiParameterDoc("begin", "开始日期")]
+        [ApiParameterDoc("end", "结束日期")]
+        [ApiResponse("天平均流量统计")]
         public AggregateFlowView Get(string collegeName, DateTime begin, DateTime end)
         {
             var cells = _collegeCellViewService.GetViews(collegeName);
@@ -76,6 +100,11 @@ namespace LtePlatform.Controllers.Kpi
         }
 
         [HttpGet]
+        [ApiDoc("查询指定学校指定日期范围内流量情况，按照日期排列")]
+        [ApiParameterDoc("collegeName", "学校名称")]
+        [ApiParameterDoc("beginDate", "开始日期")]
+        [ApiParameterDoc("endDate", "结束日期")]
+        [ApiResponse("流量情况，按照日期排列，每天一条记录")]
         public IEnumerable<FlowView> GetDateViews(string collegeName, DateTime beginDate, DateTime endDate)
         {
             var cells = _collegeCellViewService.GetViews(collegeName);
