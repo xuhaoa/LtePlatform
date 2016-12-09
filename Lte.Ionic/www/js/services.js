22b1b8174248370d6609e64caed23fff419a6f6a
ouyh18 <ouyh18@189.cn>
2016-12-09 11:43:07 +0800

    2016-12-9 Refactor...

--- Lte.Ionic/www/js/services.js
+++ Lte.Ionic/www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+﻿angular.module('starter.services', [])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
@@ -6,27 +6,27 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: '东邪',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: '西毒',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: '南帝',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: '北丐',
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: '中神通',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
   }];
