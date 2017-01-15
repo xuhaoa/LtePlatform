45e065b7bf78b053a8cbacdef829d57774bb8bf5
ouyh18 <ouyh18@189.cn>
2017-01-15 09:14:36 +0800

    2017-1-15 Refactor...

--- Lte.Evaluations/DataService/Basic/CellService.cs
+++ Lte.Evaluations/DataService/Basic/CellService.cs
@@ -52,7 +52,7 @@ namespace Lte.Evaluations.DataService.Basic
             if (cell==null) return new List<CellView>();
             return
                 GetCells(cell.Longtitute - 0.2, cell.Longtitute + 0.2, cell.Lattitute - 0.2, cell.Lattitute + 0.2)
-                    .Where(x => x.Pci == pci && sectorId < x.SectorId + 16 && x.SectorId < sectorId + 16)
+                    .Where(x => x.Pci == pci)
                     .Select(x => CellView.ConstructView(x, _eNodebRepository));
         }
 
