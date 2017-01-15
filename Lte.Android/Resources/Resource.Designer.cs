f5eae53ffa61f8f7d263eab4448e48b88db1d42c
ouyh18 <ouyh18@189.cn>
2017-01-15 15:41:10 +0800

    2017-1-15 Refactor...

--- Lte.Android/Resources/Resource.Designer.cs
+++ Lte.Android/Resources/Resource.Designer.cs
@@ -60,8 +60,17 @@ namespace Lte.Android
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int Email = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int Name = 2131034114;
+			
 			// aapt resource value: 0x7f050000
 			public const int sv = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int tvEmail = 2131034115;
 			
 			// aapt resource value: 0x7f050001
 			public const int tvName = 2131034113;
@@ -98,11 +107,11 @@ namespace Lte.Android
 			// aapt resource value: 0x7f040001
 			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
-			
 			// aapt resource value: 0x7f040002
-			public const int welcomeText = 2130968578;
+			public const int Email = 2130968578;
+			
+			// aapt resource value: 0x7f040000
+			public const int Name = 2130968576;
 			
 			static String()
 			{
