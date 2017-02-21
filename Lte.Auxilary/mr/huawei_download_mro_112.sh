74c27fc7867a63d7768045777c5dc4012a4c759f
ouyh18 <ouyh18@189.cn>
2017-02-22 03:36:35 +0800

    2017-2-22 增加华为MR下载脚本

--- Lte.Auxilary/mr/huawei_download_mro_112.sh
+++ Lte.Auxilary/mr/huawei_download_mro_112.sh
@@ -1,1 +1,1 @@
-/home/wireless/env/py3env/bin/python /home/wireless/Public/ftp_huawei_112.py
+/home/wireless/env/py3env/bin/python /home/wireless/Public/ftp_huawei.py 132.122.152.112 132.122.155.213
---
+++ Lte.Auxilary/mr/huawei_download_mrs_112_216.sh
@@ -0,0 +1,1 @@
+/home/wireless/env/py3env/bin/python /home/wireless/Public/ftp_huawei_mrs.py 132.122.152.112 132.122.155.216 MR_HW_SOURCE_D
