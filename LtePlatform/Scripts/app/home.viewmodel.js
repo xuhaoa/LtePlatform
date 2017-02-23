1ba66a472408a7a9d5da031399cd234f85d0f4e0
ouyh18 <ouyh18@189.cn>
2017-02-24 06:13:22 +0800

    2017-2-24 调整主页结构

--- LtePlatform/Scripts/app/home.viewmodel.js
+++ LtePlatform/Scripts/app/home.viewmodel.js
@@ -1,5 +1,5 @@
 ﻿angular.module("myApp", ['app.common'])
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider) {
         var viewDir = "/appViews/Home/";
         $stateProvider
             .state('list', {
@@ -17,7 +17,7 @@
             });
         $urlRouterProvider.otherwise('/');
     })
-    .run(function ($rootScope, appUrlService, appRegionService) {
+    .run(function($rootScope, appUrlService, appRegionService) {
         $rootScope.rootPath = "/#/";
 
         $rootScope.page = {
@@ -36,7 +36,7 @@
             value: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8),
             opened: false
         };
-        $rootScope.closeAlert = function (messages, index) {
+        $rootScope.closeAlert = function(messages, index) {
             messages.splice(index, 1);
         };
         var yesterday = new Date();
@@ -54,29 +54,19 @@
             options: []
         };
         appRegionService.initializeCities()
-            .then(function (result) {
+            .then(function(result) {
                 $rootScope.city.options = result;
                 $rootScope.city.selected = result[0];
             });
     })
-.controller("menu.root", function ($scope, appUrlService) {
-    var rootUrl = "/#";
+    .controller("menu.root", function($scope, appUrlService) {
+        var rootUrl = "/#";
         $scope.menuItem = {
-            displayName: "优化相关",
+            displayName: "射频优化",
             subItems: [
                 {
                     displayName: "规划辅助",
                     url: appUrlService.getPlanUrlHost() + 'guihuafuzhu/index/index.php'
-                }, {
-                    displayName: "路测分析",
-                    url: appUrlService.getDtUrlHost() + 'admin'
-                }, {
-                    displayName: "负荷评估",
-                    url: appUrlService.getParameterUrlHost() + 'ltecapability.html'
-                }, {
-                    displayName: "传统指标",
-                    url: "/Kpi",
-                    tooltip: "对传统指标（主要是2G和3G）的监控、分析和地理化呈现"
                 }, {
                     displayName: "工单管控",
                     url: "/Kpi/WorkItem",
@@ -85,43 +75,62 @@
                     displayName: "校园网专题优化",
                     url: "/College/Map",
                     tooltip: "校园网专项优化，包括数据管理、指标分析、支撑工作管理和校园网覆盖呈现"
-                }, {
-                    displayName: "4G网络质量分析",
-                    url: appUrlService.getTopnUrlHost(),
-                    tooltip: "4G网络质量分析与日常优化"
-                }, {
-                    displayName: "路测管理",
-                    url: appUrlService.getDtUrlHost2(),
-                    tooltip: "路测综合管理"
+                },
+                {
+                    displayName: "基础数据总览",
+                    url: rootUrl + "/Parameters/List"
                 }, {
                     displayName: "基础信息管理",
                     url: appUrlService.getParameterUrlHost() + 'cellInfo.html'
                 }, {
                     displayName: "边界漫游信息",
                     url: appUrlService.getParameterUrlHost() + 'lteboundary.html'
-                },
-                {
-                    displayName: "基础数据总览",
-                    url: rootUrl + "/"
-                }, {
-                    displayName: "小区地图查询",
-                    url: rootUrl + "/query"
-                }, {
-                    displayName: "专题优化管理",
-                    url: rootUrl + "/topic"
                 }
             ]
         };
     })
-.controller("home.network", function ($scope, appRegionService, networkElementService, baiduMapService, coverageDialogService,
-    geometryService) {
-    
-    baiduMapService.initializeMap("map", 11);
-    baiduMapService.addCityBoundary("佛山");
-    var colors = ['#40d3c3', '#d340c3', '#d3c340', '#40c3d3', '#c3d340', '#c340d3']
+    .controller('menu.kpi', function($scope, appUrlService) {
+        $scope.menuItem = {
+            displayName: "指标优化",
+            subItems: [
+                {
+                    displayName: "4G网络质量分析",
+                    url: appUrlService.getTopnUrlHost(),
+                    tooltip: "4G网络质量分析与日常优化"
+                }, {
+                    displayName: "指标总览",
+                    url: "/Rutrace",
+                    tooltip: "对指标的监控、分析和地理化呈现"
+                }, {
+                    displayName: "负荷评估",
+                    url: appUrlService.getParameterUrlHost() + 'ltecapability.html'
+                }
+            ]
+        }
+    })
+    .controller('menu.dt', function($scope, appUrlService) {
+        $scope.menuItem = {
+            displayName: "路测管理",
+            subItems: [
+                {
+                    displayName: "路测分析",
+                    url: appUrlService.getDtUrlHost() + 'admin'
+                }, {
+                    displayName: "路测管理",
+                    url: appUrlService.getDtUrlHost2(),
+                    tooltip: "路测综合管理"
+                }
+            ]
+        }
+    })
+    .controller("home.network", function($scope, appRegionService, networkElementService, baiduMapService, coverageDialogService,
+        geometryService) {
+        baiduMapService.initializeMap("map", 11);
+        baiduMapService.addCityBoundary("佛山");
+        var colors = ['#40d3c3', '#d340c3', '#d3c340', '#40c3d3', '#c3d340', '#c340d3']
         var city = $scope.city.selected || '佛山';
         appRegionService.queryDistricts(city).then(function(districts) {
-            angular.forEach(districts, function (district, $index) {
+            angular.forEach(districts, function(district, $index) {
                 networkElementService.queryOutdoorCellSites(city, district).then(function(sites) {
                     geometryService.transformToBaidu(sites[0].longtitute, sites[0].lattitute).then(function(coors) {
                         var xOffset = coors.x - sites[0].longtitute;
@@ -134,7 +143,7 @@
         $scope.showLteTownStats = function() {
             coverageDialogService.showTownStats(city);
         };
-        $scope.showCdmaTownStats = function () {
+        $scope.showCdmaTownStats = function() {
             coverageDialogService.showCdmaTownStats(city);
         };
         $scope.showFlow = function() {
