cd78d81cf19d8f53e95148db9b1a082df54a9154
ouyh18 <ouyh18@189.cn>
2017-05-13 22:46:55 +0800

    2017-5-13 室分对话框中增加基站告警的查询。

--- LtePlatform/Scripts/service/topic/baidu.map.js
+++ LtePlatform/Scripts/service/topic/baidu.map.js
@@ -1296,14 +1296,26 @@
 		};
 	})
 	.controller('map.distribution.dialog', function($scope, $uibModalInstance, distribution, dialogTitle, appFormatService,
-		networkElementService) {
+		networkElementService, alarmsService) {
 		$scope.distribution = distribution;
 		$scope.dialogTitle = dialogTitle;
 		$scope.distributionGroups = appFormatService.generateDistributionGroups(distribution);
+		$scope.alarmLevel = {
+		    options: ["严重告警", "重要以上告警", "所有告警"],
+		    selected: "重要以上告警"
+		};
+		$scope.alarms = [];
+		$scope.searchAlarms = function () {
+		    alarmsService.queryENodebAlarmsByDateSpanAndLevel(distribution.eNodebId,
+				$scope.beginDate.value, $scope.endDate.value, $scope.alarmLevel.selected).then(function (result) {
+				    $scope.alarms = result;
+				});
+		};
 		if (distribution.eNodebId > 0) {
 			networkElementService.queryCellInfo(distribution.eNodebId, distribution.lteSectorId).then(function(cell) {
 				$scope.lteGroups = appFormatService.generateCellGroups(cell);
 			});
+		    $scope.searchAlarms();
 		}
 		if (distribution.btsId > 0) {
 			networkElementService.queryCdmaCellInfo(distribution.btsId, distribution.cdmaSectorId).then(function(cell) {
