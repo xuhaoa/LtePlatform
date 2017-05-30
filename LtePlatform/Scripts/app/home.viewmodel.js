e64aea4941a49948b233ca940c6bd54270777edd
ouyh18 <ouyh18@189.cn>
2017-05-31 05:36:41 +0800

    2017-5-31 Refactor....

--- LtePlatform/Scripts/app/home.viewmodel.js
+++ LtePlatform/Scripts/app/home.viewmodel.js
@@ -1427,18 +1427,29 @@
         });
 
     })
-    .controller("complain.micro", function ($scope, baiduMapService, alarmsService) {
+    .controller("complain.micro", function ($scope, baiduMapService, alarmsService, dumpPreciseService, mapDialogService) {
         baiduMapService.initializeMap("map", 11);
         baiduMapService.addCityBoundary("佛山");
 
-        $scope.initializeLegend();
         alarmsService.queryMicroItems().then(function(items) {
             angular.forEach(items, function(item) {
                 var marker = baiduMapService.generateIconMarker(item.longtitute, item.lattitute, "/Content/themes/baidu/address.png");
                 baiduMapService.addOneMarkerToScope(marker, function(stat) {
-                    console.log(stat);
+                    mapDialogService.showMicroAmpliferInfos(stat);
                 }, item);
             });
+        });
+
+        $scope.districts = [];
+
+        $scope.$watch('city.selected', function (city) {
+            if (city) {
+                $scope.legend.title = city;
+                $scope.initializeLegend();
+                dumpPreciseService.generateUsersDistrict(city, $scope.districts, function (district, $index) {
+                    baiduMapService.addDistrictBoundary(district, $scope.colors[$index]);
+                });
+            }
         });
     })
 
