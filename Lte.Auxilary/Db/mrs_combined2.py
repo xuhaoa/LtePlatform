02261b48e217016e0e48a5068d57dd96ec8e7307
ouyh18 <ouyh18@189.cn>
2017-10-04 12:05:01 +0800

    2017-10-4 优化RIP MRS数据的合并规则。

--- Lte.Auxilary/Db/mrs_combined2.py
+++ Lte.Auxilary/Db/mrs_combined2.py
@@ -10,7 +10,8 @@ time=datetime.datetime.today()
 time+=datetime.timedelta(hours=-24)
 time=time.replace(hour=0,minute=0,second=0)
 datestr=time.strftime("%Y%m%d")
-mrNames=['RSRP','Tadv','PowerHeadRoom','SinrUL','TadvRsrp']
+regexNames=['ReceivedIPower']
+frequencyList=['100','1825','2446','75','1850','2452']
 
 def process_combined_general(theme,sectorId):
     Pci_List=list(db['mrs_'+theme+'_'+datestr].find({'CellId': sectorId}))
@@ -23,23 +24,26 @@ def process_combined_general(theme,sectorId):
         db['mrs_'+theme+'_combined'].insert_many(statList)
         print('CellId: ', sectorId, 'from theme '+theme, ' inserted items: ', len(statList))
 
-def process_combined(theme,eNodebId,cellId):
-    sectorId=str(eNodebId)+'-'+str(cellId)
-    process_combined_general(theme,sectorId)
+def process_combined_regex(theme,eNodebId,cellId):
+    for frequency in frequencyList:
+        for subframe in range(10):
+            sectorId=str(eNodebId)+'-'+str(cellId)+':'+frequency+':'+str(subframe)
+            process_combined_general(theme,sectorId)
 
-for theme in mrNames:
+for theme in regexNames:
     for eNodebId in range(550912, 552960):
         for cellId in range(16):
-            process_combined(theme,eNodebId,cellId)
-            process_combined(theme,eNodebId,cellId+48)
-            process_combined(theme,eNodebId,cellId+16)
+            process_combined_regex(theme,eNodebId,cellId)
+            process_combined_regex(theme,eNodebId,cellId+48)
+            process_combined_regex(theme,eNodebId,cellId+16)
     for eNodebId in range(499712, 503808):
         for cellId in range(16):
-            process_combined(theme,eNodebId,cellId)
-            process_combined(theme,eNodebId,cellId+48)
-            process_combined(theme,eNodebId,cellId+16)
+            process_combined_regex(theme,eNodebId,cellId)
+            process_combined_regex(theme,eNodebId,cellId+48)
+            process_combined_regex(theme,eNodebId,cellId+16)
     for eNodebId in range(868352,870400):
         for cellId in range(16):
-            process_combined(theme,eNodebId,cellId)
-            process_combined(theme,eNodebId,cellId+48)
-            process_combined(theme,eNodebId,cellId+16)
+            process_combined_regex(theme,eNodebId,cellId)
+            process_combined_regex(theme,eNodebId,cellId+48)
+            process_combined_regex(theme,eNodebId,cellId+16)
+
