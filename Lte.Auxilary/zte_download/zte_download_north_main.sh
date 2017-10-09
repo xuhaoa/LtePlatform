9020a99861247927001bc106ad3ac7968c1d0849
ouyh18 <ouyh18@189.cn>
2017-10-09 14:15:22 +0800

    2017-10-9 Refactor...

--- Lte.Auxilary/zte_download/zte_download_north_main.sh
+++ Lte.Auxilary/zte_download/zte_download_north_main.sh
@@ -4,3 +4,4 @@
 /home/wireless/env/py3env/bin/python /home/wireless/North/north_zte.py 132.122.151.232
 /home/wireless/env/py3env/bin/python /home/wireless/North/north_zte.py 132.122.155.137
 /home/wireless/env/py3env/bin/python /home/wireless/North/north_zte.py 132.122.155.135
+/home/wireless/env/py3env/bin/python /home/wireless/North/north_zte.py 132.122.151.115
